568afb4e6f7864c1ee127038cbb5e829c0bece44
Yu <583181285@qq.com>
2017-01-03 11:42:42 +0800

    new, update toc

--- src/toc.ts
+++ src/toc.ts
@@ -4,7 +4,7 @@
  * Modified from <https://github.com/AlanWalk/Markdown-TOC>
  */
 
-import { commands, window, workspace, ExtensionContext } from 'vscode';
+import { commands, window, workspace, ExtensionContext, Position, Range, TextDocument } from 'vscode';
 
 const prefix = 'markdown.extension.toc.';
 
@@ -15,13 +15,15 @@ const REGEXP_CODE_BLOCK = /^```/;
  * Workspace config
  */
 let wsConfig = { tab: '    ', eol: '\r\n' };
-let tocConfig = { depth: 6, orderedList: false };
+let tocConfig = { depth: 6, orderedList: false, updateOnSave: false };
+
+let alreadyUpdated = false; // Prevent update TOC again after manually calling `doc.save()`
 
 export function activate(context: ExtensionContext) {
     const cmds: Command[] = [
         { command: 'create', callback: createToc },
-        { command: 'update', callback: updateToc },
-        { command: 'delete', callback: deleteToc }
+        { command: 'update', callback: updateToc }
+        // , { command: 'delete', callback: deleteToc }
     ].map(cmd => {
         cmd.command = prefix + cmd.command;
         return cmd;
@@ -30,25 +32,42 @@ export function activate(context: ExtensionContext) {
     cmds.forEach(cmd => {
         context.subscriptions.push(commands.registerCommand(cmd.command, cmd.callback));
     });
+
+    context.subscriptions.push(workspace.onDidSaveTextDocument(onSave));
 }
 
 function createToc() {
-    updateToc();
+    loadConfig();
+
+    let editor = window.activeTextEditor;
+    editor.edit(function (editBuilder) {
+        editBuilder.insert(editor.selection.active, generateTocText());
+    });
 }
 
 function updateToc() {
-    let editor = window.activeTextEditor;
+    let range = detectTocRange();
+    if (range != null) {
+        let anchor = range.start;
+        window.activeTextEditor.edit(editBuilder => {
+            editBuilder.delete(range);
+            editBuilder.insert(anchor, generateTocText());
+        });
+    }
+}
 
-    loadConfig();
+function deleteToc() {
+    // Pass
+}
 
-    // Generate TOC
+function generateTocText() {
     let toc = [];
     let headingList = getHeadingList();
     let startDepth = 6; // In case that there is no heading in level 1.
     headingList.forEach(heading => {
         if (heading.level < startDepth) startDepth = heading.level;
     });
-    let order = new Array(tocConfig.depth - startDepth + 1).fill(0); // Ordered list
+    let order = new Array(tocConfig.depth - startDepth + 1).fill(0); // Used for ordered list
     headingList.forEach(heading => {
         if (heading.level <= tocConfig.depth) {
             let indentation = heading.level - startDepth;
@@ -61,14 +80,42 @@ function updateToc() {
             if (tocConfig.orderedList) order.fill(0, indentation + 1);
         }
     });
-
-    editor.edit(function (editBuilder) {
-        editBuilder.insert(editor.selection.active, toc.join(wsConfig.eol));
-    });
+    return toc.join(wsConfig.eol);
 }
 
-function deleteToc() {
+function detectTocRange() {
+    console.log('Detecting TOC ...');
 
+    let doc = window.activeTextEditor.document;
+    let start, end: Position;
+    let headings = getHeadingList();
+
+    if (headings.length == 0) return;
+
+    for (let index = 0; index < doc.lineCount; index++) {
+        let lineText = doc.lineAt(index).text;
+        if (start == null) { // No line matched with start yet
+            if (lineText.startsWith('- ') || lineText.startsWith('1. ')) {
+                lineText = lineText.replace(/^- /, '').replace(/^\d\. /, '');
+                if (lineText.startsWith(headings[0].title)) {
+                    start = new Position(index, 0);
+                    console.log(`Start: Ln ${start.line + 1}, Col ${start.character + 1}`);
+                }
+            }
+        } else { // Start line already found
+            lineText = lineText.trim();
+            if (!(lineText.startsWith('- ') || lineText.match(/^\d\. /))) { // End of a list block
+                end = new Position(index - 1, doc.lineAt(index - 1).text.length);
+                console.log(`End:   Ln ${end.line + 1}, Col ${end.character + 1}`);
+                break;
+            }
+        }
+    }
+    if ((start != null) && (end != null)) {
+        return new Range(start, end);
+    }
+    console.log('No TOC detected.')
+    return null;
 }
 
 function getHeadingList() {
@@ -99,6 +146,17 @@ function getHeadingList() {
     return headingList;
 }
 
+function onSave(doc: TextDocument) {
+    if (alreadyUpdated) {
+        alreadyUpdated = false;
+        return;
+    } else if (doc.languageId == 'markdown') {
+        updateToc();
+        alreadyUpdated = true;
+        doc.save();
+    }
+}
+
 function loadConfig() {
     // Workspace config
     wsConfig.eol = <string>workspace.getConfiguration("files").get("eol");
@@ -113,4 +171,5 @@ function loadConfig() {
     // TOC config
     tocConfig.depth = <number>workspace.getConfiguration('markdown.extension.toc').get('depth');
     tocConfig.orderedList = <boolean>workspace.getConfiguration('markdown.extension.toc').get('orderedList');
+    tocConfig.updateOnSave = <boolean>workspace.getConfiguration('markdown.extension.toc').get('updateOnSave');
 }
