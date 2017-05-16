9298ff692fba5dbe6fc5b7c4a225b98e512ddcf6
Yu <583181285@qq.com>
2017-05-16 16:27:05 +0800

    🐛 ctrl+enter

--- src/listEditing.ts
+++ src/listEditing.ts
@@ -1,37 +1,61 @@
 'use strict'
 
-import { commands, window, workspace, ExtensionContext, Range } from 'vscode';
+import { commands, window, workspace, ExtensionContext, Range, Selection } from 'vscode';
 import * as vscode from 'vscode';
 
 export function activate(context: ExtensionContext) {
     context.subscriptions.push(commands.registerCommand('markdown.extension.onEnterKey', onEnterKey));
+    context.subscriptions.push(commands.registerCommand('markdown.extension.onCtrlEnterKey', () => { onEnterKey('ctrl'); }));
     context.subscriptions.push(commands.registerCommand('markdown.extension.onTabKey', onTabKey));
     context.subscriptions.push(commands.registerCommand('markdown.extension.onBackspaceKey', onBackspaceKey));
 }
 
-function onEnterKey() {
+function onEnterKey(modifiers?: string) {
     let editor = window.activeTextEditor;
     let cursorPos = editor.selection.active;
-    let textBeforeCursor = editor.document.lineAt(cursorPos.line).text.substr(0, cursorPos.character);
+    let line = editor.document.lineAt(cursorPos.line);
+    let textBeforeCursor = line.text.substr(0, cursorPos.character);
+
+    let lineBreakPos = cursorPos;
+    if (modifiers == 'ctrl') {
+        lineBreakPos = line.range.end;
+    }
 
     let matches;
-    if ((matches = /(\s*[-\+\*] ).+/.exec(textBeforeCursor)) !== null) { // Unordered list
+    if ((matches = /(\s*[-\+\*] ).+/.exec(textBeforeCursor)) !== null) {
+        // Unordered list
         editor.edit(editBuilder => {
-            editBuilder.insert(cursorPos, `\n${matches[1]}`);
+            editBuilder.insert(lineBreakPos, `\n${matches[1]}`);
         });
-    } else if ((matches = /(\s*)([0-8])([\.\)] )(.+)/.exec(textBeforeCursor)) !== null) { // Ordered list
+        // Fix cursor position
+        if (modifiers == 'ctrl' && !cursorPos.isEqual(lineBreakPos)) {
+            let newCursorPos = cursorPos.with(line.lineNumber + 1, matches[1].length);
+            editor.selection = new Selection(newCursorPos, newCursorPos);
+        }
+    } else if ((matches = /(\s*)([0-8])([\.\)] ).+/.exec(textBeforeCursor)) !== null) {
+        // Ordered list
         let config = workspace.getConfiguration('markdown.extension.orderedList').get<string>('marker');
         let marker = '1';
         if (config == 'ordered') {
             marker = String(Number(matches[2]) + 1);
         }
         editor.edit(editBuilder => {
-            editBuilder.insert(cursorPos, `\n${matches[1] + marker + matches[3]}`);
+            editBuilder.insert(lineBreakPos, `\n${matches[1] + marker + matches[3]}`);
         });
+        // Fix cursor position
+        if (modifiers == 'ctrl' && !cursorPos.isEqual(lineBreakPos)) {
+            let newCursorPos = cursorPos.with(line.lineNumber + 1, (matches[1] + marker + matches[3]).length);
+            editor.selection = new Selection(newCursorPos, newCursorPos);
+        }
     } else {
-        editor.edit(editBuilder => {
-            editBuilder.insert(cursorPos, '\n');
-        });
+        if (modifiers == 'ctrl') {
+            commands.executeCommand('editor.action.insertLineAfter');
+            return;
+        } else {
+            editor.edit(editBuilder => {
+                editBuilder.insert(lineBreakPos, '\n');
+            });
+        }
     }
 }
 
