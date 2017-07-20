5bde7b631f49d40586031c8ea10f46b964e2cf0e
Yu <583181285@qq.com>
2017-07-20 11:07:54 +0800

    💩 bad trial

--- test/formatting.test.ts
+++ test/formatting.test.ts
@@ -1,46 +1,33 @@
 import * as assert from 'assert';
 
 import * as vscode from 'vscode';
-import { Position, Selection, Uri } from 'vscode';
-import * as myExtension from '../src/extension';
+import { commands, window, Position, Range, Selection, Uri } from 'vscode';
+import * as path from 'path'
 
-// function testFormatting(lines: string[], selection: Selection, expectedLines: string[], expectedSelection: Selection) {
-//     let textEditor: vscode.TextEditor;
-//     let textDocument: vscode.TextDocument;
-//     return vscode.workspace.openTextDocument(Uri.parse('untitled:d:/test.md')).then(document => {
-//         textDocument = document;
-//         return vscode.window.showTextDocument(textDocument);
-//     }).then(editor => {
-//         assert(vscode.window.activeTextEditor, 'No active editor');
-//         vscode.window.activeTextEditor.edit(editBuilder => {
-//             editBuilder.insert(new Position(0, 0), lines.join());
-//         });
-//         return editor;
-//     }).then(editor => {
-//         vscode.window.activeTextEditor.document.save();
-//         console.log('--------------------------------------------');
-//         assert.equal(-1, 1);
-//     });
-// }
+let testMdFile = path.join(__dirname, '..', '..', 'test', 'test.md');
 
-suite("Formatting (italic, bold ...)", () => {
-
-    test("Toggle bold", () => {
-        let textEditor: vscode.TextEditor;
-        let textDocument: vscode.TextDocument;
-        return vscode.workspace.openTextDocument(Uri.parse('untitled:d:/test.md')).then(document => {
-            textDocument = document;
-            return vscode.window.showTextDocument(textDocument);
-        }).then(editor => {
-            assert(vscode.window.activeTextEditor, 'No active editor');
-            vscode.window.activeTextEditor.edit(editBuilder => {
-                editBuilder.insert(new Position(0, 0), ["line1","line2"].join('\n'));
-            });
-            return editor;
-        }).then(editor => {
-            vscode.window.activeTextEditor.document.save();
-            console.log('--------------------------------------------');
-            assert.equal(-1, 1);
+function testCommand(command: string, lines: string[], selection: Selection, expLines: string[], expSelection: Selection) {
+    return vscode.workspace.openTextDocument(testMdFile).then(doc => {
+        return vscode.window.showTextDocument(doc);
+    }).then(editor => {
+        assert(vscode.window.activeTextEditor, 'No active editor');
+        return editor.edit(editBuilder => {
+            let fullRange = new Range(new Position(0, 0), editor.document.positionAt(editor.document.getText().length));
+            editBuilder.delete(fullRange);
+            editBuilder.insert(new Position(0, 0), lines.join('\n'));
         });
+    }).then(() => {
+        window.activeTextEditor.selection = selection;
+        return commands.executeCommand(command);
+    }).then(() => {
+        assert.deepEqual(window.activeTextEditor.document.getText(), expLines.join('\n'));
+        assert.deepEqual(window.activeTextEditor.selection, expSelection);
+    });
+}
+
+suite("Formatting (italic, bold ...).", () => {
+
+    test("Toggle bold", done => {
+        testCommand('markdown.extension.editing.toggleItalic', ['text'], new Selection(0, 2, 0, 2), ['****text'], new Selection(0, 2, 0, 2)).then(done, done);
     });
 });
