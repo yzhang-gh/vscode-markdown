c909eac273542cfea42c08ef62bb78af371fb4d1
Yu <583181285@qq.com>
2017-07-25 22:12:31 +0800

    🎨

--- test/formatting.test.ts
+++ test/formatting.test.ts
@@ -1,58 +1,9 @@
-import * as assert from 'assert';
-
-import { commands, window, workspace, Position, Range, Selection, Uri } from 'vscode';
-import * as path from 'path'
-
-let testMdFile = path.join(__dirname, '..', '..', 'test', 'test.md');
-let defaultConfigs = {
-    "markdown.extension.toc.depth": 6,
-    "markdown.extension.toc.orderedList": false,
-    "markdown.extension.toc.plaintext": false,
-    "markdown.extension.toc.updateOnSave": true,
-    "markdown.extension.preview.autoShowPreviewToSide": false,
-    "markdown.extension.orderedList.marker": "one",
-    "markdown.extension.italic.indicator": "*",
-    "markdown.extension.quickStyling": false
-}
-
-// 💩 Promise, then, async/await ... <https://github.com/Microsoft/vscode/issues/31210>
-
-async function testCommand(command: string, configs, lines: string[], selection: Selection, expLines: string[], expSelection: Selection) {
-    let tempConfigs = defaultConfigs;
-    for (let key in configs) {
-        if (configs.hasOwnProperty(key)) {
-            tempConfigs[key] = configs[key];
-        }
-    }
-    for (let key in tempConfigs) {
-        if (tempConfigs.hasOwnProperty(key)) {
-            await workspace.getConfiguration().update(key, tempConfigs[key], true);
-        }
-    }
-    return workspace.openTextDocument(testMdFile).then(document => {
-        return window.showTextDocument(document).then(editor => {
-            return editor.edit(editBuilder => {
-                let fullRange = new Range(new Position(0, 0), editor.document.positionAt(editor.document.getText().length));
-                editBuilder.delete(fullRange);
-                editBuilder.insert(new Position(0, 0), lines.join('\n'));
-            }).then(b => {
-                window.activeTextEditor.selection = selection;
-                return commands.executeCommand(command).then(num => {
-                    assert.deepEqual(window.activeTextEditor.document.getText(), expLines.join('\n'));
-                    assert.deepEqual(window.activeTextEditor.selection, expSelection);
-                });
-            });
-        });
-    });
-}
-
-function toString(s: Selection): string {
-    return `(${s.start.line}, ${s.start.character}, ${s.end.line}, ${s.end.character})`;
-}
+import { workspace, Selection } from 'vscode';
+import { testMdFile, defaultConfigs, testCommand } from './testUtils';
 
 suite("Formatting.", () => {
     suiteSetup(async () => {
-        // 💩 Preload file to prevent the first test timeout
+        // 💩 Preload file to prevent the first test to be treated timeout
         await workspace.openTextDocument(testMdFile);
 
         for (let key in defaultConfigs) {
@@ -111,5 +62,4 @@ suite("Formatting.", () => {
     test("Toggle italic. Use `_`", done => {
         testCommand('markdown.extension.editing.toggleItalic', { 'markdown.extension.italic.indicator': '_' }, ['text'], new Selection(0, 0, 0, 4), ['_text_'], new Selection(0, 0, 0, 6)).then(done, done);
     });
-
 });
---
+++ test/testUtils.ts
@@ -0,0 +1,48 @@
+import * as path from 'path'
+import * as assert from 'assert';
+import { commands, window, workspace, Position, Range, Selection } from 'vscode';
+
+export let testMdFile = path.join(__dirname, '..', '..', 'test', 'test.md');
+export let defaultConfigs = {
+    "markdown.extension.toc.levels": "1..6",
+    "markdown.extension.toc.orderedList": false,
+    "markdown.extension.toc.plaintext": false,
+    "markdown.extension.toc.updateOnSave": true,
+    "markdown.extension.preview.autoShowPreviewToSide": false,
+    "markdown.extension.orderedList.marker": "one",
+    "markdown.extension.italic.indicator": "*",
+    "markdown.extension.quickStyling": false
+}
+
+// 💩 Promise, then, async/await ... <https://github.com/Microsoft/vscode/issues/31210>
+
+export async function testCommand(command: string, configs, lines: string[], selection: Selection, expLines: string[], expSelection: Selection) {
+    let tempConfigs = defaultConfigs;
+    for (let key in configs) {
+        if (configs.hasOwnProperty(key)) {
+            tempConfigs[key] = configs[key];
+        }
+    }
+    for (let key in tempConfigs) {
+        if (tempConfigs.hasOwnProperty(key)) {
+            await workspace.getConfiguration().update(key, tempConfigs[key], true);
+        }
+    }
+    return workspace.openTextDocument(testMdFile).then(document => {
+        return window.showTextDocument(document).then(editor => {
+            return editor.edit(editBuilder => {
+                let fullRange = new Range(new Position(0, 0), editor.document.positionAt(editor.document.getText().length));
+                editBuilder.delete(fullRange);
+                editBuilder.insert(new Position(0, 0), lines.join('\n'));
+            }).then(b => {
+                window.activeTextEditor.selection = selection;
+                return commands.executeCommand(command).then(() => {
+                    let actual = window.activeTextEditor.document.getText();
+                    actual = actual.replace(/\r\n/g, '\n'); /* !!! */
+                    assert.deepEqual(actual, expLines.join('\n'));
+                    assert.deepEqual(window.activeTextEditor.selection, expSelection);
+                });
+            });
+        });
+    });
+}
