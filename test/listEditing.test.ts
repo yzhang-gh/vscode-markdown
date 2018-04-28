ba7f2fdf1459320dc8a14ac5f7670baf40b20b66
Eric <eric@ericyd.com>
2018-04-28 10:21:43 -0700

    add ordered list tab key tests

--- test/listEditing.test.ts
+++ test/listEditing.test.ts
@@ -76,4 +76,16 @@ suite("List editing.", () => {
     test("Tab key. 3: '- [ ] |'", done => {
         testCommand('markdown.extension.onTabKey', { "editor.insertSpaces": true, "editor.tabSize": 4 }, ['- [ ] item1'], new Selection(0, 6, 0, 6), ['    - [ ] item1'], new Selection(0, 10, 0, 10)).then(done, done);
     });
+
+    test("Tab key. 4: '2. |'", done => {
+        testCommand('markdown.extension.onTabKey', { "editor.insertSpaces": true, "editor.tabSize": 4 }, ['2. item1'], new Selection(0, 2, 0, 2), ['    1. item1'], new Selection(0, 6, 0, 6)).then(done, done);
+    });
+
+    test("Tab key. 5: '2.  |'", done => {
+        testCommand('markdown.extension.onTabKey', { "editor.insertSpaces": true, "editor.tabSize": 4 }, ['2.  item1'], new Selection(0, 3, 0, 3), ['    1.  item1'], new Selection(0, 7, 0, 7)).then(done, done);
+    });
+
+    test("Tab key. 6: '2. [ ] |'", done => {
+        testCommand('markdown.extension.onTabKey', { "editor.insertSpaces": true, "editor.tabSize": 4 }, ['2. [ ] item1'], new Selection(0, 6, 0, 6), ['    1. [ ] item1'], new Selection(0, 10, 0, 10)).then(done, done);
+    });
 });
