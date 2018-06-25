b5ec0d455f9bb0c439d0b2e103bb96df6b6d7432
rbolsius <roger.bolsius@gmail.com>
2018-06-24 20:17:47 -0500

    🔀 Auto renumbering improvements (#210)
    * Improvements to automatic renumbering of lists
    
    * Improvements to automatic renumbering of lists
    
    * Add .vscode-tests directory to .gitignore
    
    * Fix problems that cause existing tests to fail and add new tests

--- test/listEditing.test.ts
+++ test/listEditing.test.ts
@@ -47,6 +47,14 @@ suite("List editing.", () => {
         testCommand('markdown.extension.onEnterKey', {}, ['- [ ] item1'], new Selection(0, 11, 0, 11), ['- [ ] item1', '- [ ] '], new Selection(1, 6, 1, 6)).then(done, done);
     });
 
+    test("Enter key. Keep list item text indentation. '1.  item1|'", done => {
+        testCommand('markdown.extension.onEnterKey', {}, ['1.  item1'], new Selection(0, 9, 0, 9), ['1.  item1', '2.  '], new Selection(1, 4, 1, 4)).then(done, done);
+    });
+
+    test("Enter key. Keep list item text indentation. '9.  item9|'", done => {
+        testCommand('markdown.extension.onEnterKey', {}, ['9.  item9'], new Selection(0, 9, 0, 9), ['9.  item9', '10. '], new Selection(1, 4, 1, 4)).then(done, done);
+    });
+
     test("Enter key. '- [test]|'. #122", done => {
         testCommand('markdown.extension.onEnterKey', {}, ['- [test]'], new Selection(0, 8, 0, 8), ['- [test]', '- '], new Selection(1, 2, 1, 2)).then(done, done);
     });
@@ -133,6 +141,26 @@ suite("List editing.", () => {
             new Selection(3, 3, 3, 3)).then(done, done);
     });
 
+    test("Backspace key. Fix ordered marker. 5: Selection range", done => {
+        testCommand('markdown.extension.onBackspaceKey', {},
+            [
+                '1. item1',
+                '2. item2',
+                '    1. item1-1',
+                '    2. item1-2',
+                '    3. item1-3',
+                '3. item3'
+            ],
+            new Selection(1, 0, 3, 0),
+            [
+                '1. item1',
+                '    1. item1-2',
+                '    2. item1-3',
+                '2. item3'
+            ],
+            new Selection(1, 0, 1, 0)).then(done, done);
+    });
+
     test("Tab key. 1: '- |'", done => {
         testCommand('markdown.extension.onTabKey', {}, ['- item1'], new Selection(0, 2, 0, 2), ['    - item1'], new Selection(0, 6, 0, 6)).then(done, done);
     });
@@ -193,6 +221,26 @@ suite("List editing.", () => {
             new Selection(3, 7, 3, 7)).then(done, done);
     });
 
+    test("Tab key. Fix ordered marker. 5: Selection range", done => {
+        testCommand('markdown.extension.onTabKey', {},
+            [
+                '1. test',
+                '2. test',
+                '    1. test',
+                '    2. test',
+                '3. test'
+            ],
+            new Selection(1, 0, 3, 0),
+            [
+                '1. test',
+                '    1. test',
+                '        1. test',
+                '    2. test',
+                '2. test'
+            ],
+            new Selection(1, 0, 3, 0)).then(done, done);
+    });
+
     test("Move Line Up. 1: '2. |'", done => {
         testCommand('markdown.extension.onMoveLineUp', {}, ['1. item1', '2. item2'], new Selection(1, 3, 1, 3), ['1. item2', '2. item1'], new Selection(0, 3, 0, 3)).then(done, done);
     });
@@ -216,4 +264,105 @@ suite("List editing.", () => {
     test("Move Line Down. 3: '1. [ ] |'", done => {
         testCommand('markdown.extension.onMoveLineDown', {}, ['1. [ ] item1', '2. [ ] item2'], new Selection(0, 0, 0, 0), ['1. [ ] item2', '2. [ ] item1'], new Selection(1, 0, 1, 0)).then(done, done);
     });
+
+    test("Copy Line Up. 1: '2. |'", done => {
+        testCommand('markdown.extension.onCopyLineUp', {}, ['1. item1', '2. item2'], new Selection(1, 3, 1, 3), ['1. item1', '2. item2', '3. item2'], new Selection(1, 3, 1, 3)).then(done, done);
+    });
+
+    test("Copy Line Up. 2: '2.  |'", done => {
+        testCommand('markdown.extension.onCopyLineUp', {}, ['1.  item1', '2.  item2'], new Selection(1, 3, 1, 3), ['1.  item1', '2.  item2', '3.  item2'], new Selection(1, 3, 1, 3)).then(done, done);
+    });
+
+    test("Copy Line Up. 3: '2. [ ] |'", done => {
+        testCommand('markdown.extension.onCopyLineUp', {}, ['1. [ ] item1', '2. [x] item2'], new Selection(1, 0, 1, 0), ['1. [ ] item1', '2. [x] item2', '3. [x] item2'], new Selection(1, 0, 1, 0)).then(done, done);
+    });
+
+    test("Copy Line Down. 1: '1. |'", done => {
+        testCommand('markdown.extension.onCopyLineDown', {}, ['1. item1', '2. item2'], new Selection(0, 3, 0, 3), ['1. item1', '2. item1', '3. item2'], new Selection(1, 3, 1, 3)).then(done, done);
+    });
+
+    test("Copy Line Down. 2: '1.  |'", done => {
+        testCommand('markdown.extension.onCopyLineDown', {}, ['1.  item1', '2.  item2'], new Selection(0, 3, 0, 3), ['1.  item1', '2.  item1', '3.  item2'], new Selection(1, 3, 1, 3)).then(done, done);
+    });
+
+    test("Copy Line Down. 3: '1. [ ] |'", done => {
+        testCommand('markdown.extension.onCopyLineDown', {}, ['1. [x] item1', '2. [ ] item2'], new Selection(0, 0, 0, 0), ['1. [x] item1', '2. [x] item1', '3. [ ] item2'], new Selection(1, 0, 1, 0)).then(done, done);
+    });
+
+    test("Indent Lines. 1: No selection range", done => {
+        testCommand('markdown.extension.onIndentLines', {},
+            [
+                '1. test',
+                '2. test',
+                '    1. test',
+                '3. test'
+            ],
+            new Selection(1, 5, 1, 5),
+            [
+                '1. test',
+                '    1. test',
+                '    2. test',
+                '2. test'
+            ],
+            new Selection(1, 9, 1, 9)).then(done, done);
+    });
+
+    test("Indent Lines. 2: Selection range", done => {
+        testCommand('markdown.extension.onIndentLines', {},
+            [
+                '1. test',
+                '2. test',
+                '3. test',
+                '    1. test',
+                '4. test'
+            ],
+            new Selection(1, 0, 3, 0),
+            [
+                '1. test',
+                '    1. test',
+                '    2. test',
+                '    3. test',
+                '2. test'
+            ],
+            new Selection(1, 0, 3, 0)).then(done, done);
+    });
+
+    test("Outdent Lines. 1: No selection range", done => {
+        testCommand('markdown.extension.onOutdentLines', {},
+            [
+                '1. test',
+                '    1. test',
+                '    2. test',
+                '2. test'
+            ],
+            new Selection(1, 0, 1, 0),
+            [
+                '1. test',
+                '2. test',
+                '    1. test',
+                '3. test'
+            ],
+            new Selection(1, 0, 1, 0)).then(done, done);
+    });
+
+    test("Outdent Lines. 2: Selection range", done => {
+        testCommand('markdown.extension.onOutdentLines', {},
+            [
+                '1. test',
+                '    1. test',
+                '    2. test',
+                '    3. test',
+                '2. test'
+            ],
+            new Selection(1, 0, 3, 0),
+            [
+                '1. test',
+                '2. test',
+                '3. test',
+                '    1. test',
+                '4. test'
+            ],
+            new Selection(1, 0, 3, 0)).then(done, done);
+    });
+
 });
