1c1272202c115c29a27fc1d637aa5eb67e97fa3d
Yu <583181285@qq.com>
2018-09-14 16:08:05 +0100

    🐢 increase test time tolerance

--- test/index.ts
+++ test/index.ts
@@ -17,7 +17,7 @@ var testRunner = require('vscode/lib/testrunner');
 testRunner.configure({
     ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
     useColors: true, // colored output from test results
-    timeout: 3000
+    timeout: 4000
 });
 
 module.exports = testRunner;
