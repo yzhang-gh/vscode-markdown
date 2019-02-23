bc50362b9bf86e298bd455c3cb7fad42dc550a45
yzhang <7588612+yzhang-gh@users.noreply.github.com>
2019-02-23 16:35:31 +0000

    🐛 #382

--- src/formatting.ts
+++ src/formatting.ts
@@ -118,7 +118,7 @@ function toggleUnorderedList() {
 
 function paste() {
     return env.clipboard.readText().then(text => {
-        if (/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/.test(text)) {
+        if (/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/.test(text)) {
             return commands.executeCommand("editor.action.insertSnippet", { "snippet": `[$TM_SELECTED_TEXT$0](${text})` });
         } else {
             return commands.executeCommand("editor.action.clipboardPasteAction");
