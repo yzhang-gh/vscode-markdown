5c08ed752148dc3c4bcafdfedac8baeaf5693e54
yzhang <7588612+yzhang-gh@users.noreply.github.com>
2019-03-05 20:36:39 +0000

    🐜 paste, don't create link if not single line

--- src/formatting.ts
+++ src/formatting.ts
@@ -116,14 +116,14 @@ function toggleUnorderedList() {
     }
 }
 
-function paste() {
-    return env.clipboard.readText().then(text => {
+async function paste() {
+    if (window.activeTextEditor.selection.isSingleLine) {
+        const text = await env.clipboard.readText();
         if (/^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/.test(text)) {
             return commands.executeCommand("editor.action.insertSnippet", { "snippet": `[$TM_SELECTED_TEXT$0](${text})` });
-        } else {
-            return commands.executeCommand("editor.action.clipboardPasteAction");
         }
-    });
+    }
+    return commands.executeCommand("editor.action.clipboardPasteAction");
 }
 
 function styleByWrapping(startPattern, endPattern?) {
