e68de4c07ec9d5c5488f682fdfc143a8cddf9b2e
or <igor_deyawka@mail.ru>
2019-03-17 01:40:22 +0200

    link recognition tests added
    This tests shows current recognition behavior.

--- src/formatting.ts
+++ src/formatting.ts
@@ -134,7 +134,7 @@ async function paste() {
  *
  * @return boolean
  */
-function isSingleLink(text: string) {
+export function isSingleLink(text: string) {
     const linkRegex = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
     const isLink = (linkRegex.test(text));
 
