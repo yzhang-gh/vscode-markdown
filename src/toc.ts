d77ab78389bc880a225f2732d3362c4df7217045
Scott Meesseman <48744816+spmeesseman@users.noreply.github.com>
2019-05-18 16:26:34 -0400

    🔀 Anchorlink casing (#450)
    * fix: keep same case when creating toc anchor reference links
    * chore: apply slugify() call change
    * test: chnage toc test after casing change

--- src/toc.ts
+++ src/toc.ts
@@ -93,7 +93,8 @@ async function generateTocText(doc: vscode.TextDocument): Promise<string> {
             let row = [
                 docConfig.tab.repeat(relativeLvl),
                 (tocConfig.orderedList ? (orderedListMarkerIsOne ? '1' : ++order[relativeLvl]) + '.' : tocConfig.listMarker) + ' ',
-                tocConfig.plaintext ? entryText : `[${entryText}](#${slugify(anchorText)})`
+                tocConfig.plaintext ? entryText : `[${entryText}](#${slugify(anchorText, true)})`
+                //tocConfig.plaintext ? entryText : `[${entryText}](#${slugify(anchorText)})`
             ];
             toc.push(row.join(''));
             if (tocConfig.orderedList) order.fill(0, relativeLvl + 1);
