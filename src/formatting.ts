886d813ae014cbb11d4c5f70377bd34138028bf2
sean10 <sean10reborn@gmail.com>
2020-09-13 01:29:08 +0800

    delete default quote keybinding, use better every() replace redundant code, use eol to replace \n to fix different eol problem, fixredundant quote syntax

--- src/formatting.ts
+++ src/formatting.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { commands, env, ExtensionContext, Position, Range, Selection, SnippetString, TextDocument, TextEditor, window, workspace, WorkspaceEdit } from 'vscode';
+import { commands, env, ExtensionContext, Position, Range, Selection, SnippetString, TextDocument, TextEditor, window, workspace, WorkspaceEdit,EndOfLine } from 'vscode';
 import { fixMarker } from './listEditing';
 
 export function activate(context: ExtensionContext) {
@@ -54,36 +54,26 @@ enum QuoteState {
 }
 
 function getQuoteState(lineArray: Array<string>): QuoteState {
-    let cntQuoted = 0;
-    console.log("array:", lineArray)
-    lineArray.forEach(function(line) {
-        if (line.startsWith(">")) {
-            cntQuoted += 1;
-        }
-    })
-
-    return cntQuoted != lineArray.length ? QuoteState.PARTIALQUOTED : QuoteState.FULLQUOTED;
+    return lineArray.every((line)=>{ return line.startsWith(">"); }) ? QuoteState.FULLQUOTED : QuoteState.PARTIALQUOTED;
 }
 
 function quoteLine(line: string): string {
-    if(!line.startsWith("> ") && line.startsWith(">")) {
-        return line.replace(/>/g, "> ");
-    } else if(!line.startsWith("> ")){
-        return "> " + line;
+    if(!line.startsWith(">")){
+        return ">" + line;
     } else {
         return line;
     }
 }
 
 function unquoteLine(line: string): string {
-    if (line.startsWith("> ")) {
-        return line.slice(2);
+    if (line.startsWith(">")) {
+        return line.slice(1);
     } else {
         return line;
     }
 }
 
-function setQuote(lineArray: Array<string>, state: QuoteState): string {
+function setQuote(lineArray: Array<string>, state: QuoteState, eol: string): string {
     let resultArray;
     switch(state) {
         case QuoteState.PARTIALQUOTED:
@@ -95,21 +85,24 @@ function setQuote(lineArray: Array<string>, state: QuoteState): string {
         default:
             resultArray = lineArray;
     }
-    return resultArray.join('\n');
+    return resultArray.join(eol);
 }
 
-async function toggleQuote() {
+function toggleQuote() {
     let editor = window.activeTextEditor;
     let start = editor.selection.start;
     let end = editor.selection.end;
     start = editor.document.lineAt(start.line).range.start;
+    end = editor.document.lineAt(end.line).range.end;
     let linesText = editor.document.getText(new Range(start, end));;
+    let eol = editor.document.eol === EndOfLine.CRLF ? '\r\n' : '\n';
 
-    let line_array = linesText.split('\n');
+    let line_array = linesText.split(eol);
     let state = getQuoteState(line_array);
-    let resultText = setQuote(line_array, state);
+    console.log("state", state);
+    let resultText = setQuote(line_array, state, eol);
 
-    return await editor.edit(editBuilder => {
+    return editor.edit(editBuilder => {
         editBuilder.replace(new Range(start, end), resultText);
     });
 }
