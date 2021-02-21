c21becdb840c023928fbaae149f3ec06cdbebdb8
Lemmingh <43396014+Lemmingh@users.noreply.github.com>
2021-02-22 00:12:21 +0800

    Move LaTeX commands to a new file
    Naming needs further discussion.

--- src/completion/index.ts
+++ src/completion/index.ts
@@ -2,9 +2,10 @@ import * as fs from 'fs';
 import * as path from "path";
 import * as vscode from "vscode";
 import sizeOf from 'image-size';
-import { getAllTocEntry, IHeading } from './toc';
-import { mathEnvCheck } from "./util/contextCheck";
-import { Document_Selector_Markdown } from './util/generic';
+import { getAllTocEntry, IHeading } from "../toc";
+import { mathEnvCheck } from "../util/contextCheck";
+import { Document_Selector_Markdown } from "../util/generic";
+import { Katex_Command_Completion_Items } from "./latex";
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider(Document_Selector_Markdown, new MdCompletionItemProvider(), '(', '\\', '/', '[', '#'));
@@ -14,365 +15,11 @@ type CompletionItemProviderResult = Promise<vscode.CompletionItem[] | vscode.Com
 
 class MdCompletionItemProvider implements vscode.CompletionItemProvider {
 
-    // Suffixes explained:
-    // \cmd         -> 0
-    // \cmd{$1}     -> 1
-    // \cmd{$1}{$2} -> 2
-    //
-    // Use linebreak to mimic the structure of the KaTeX [Support Table](https://katex.org/docs/supported.html)
-    accents1 = [
-        'tilde', 'mathring',
-        'widetilde', 'overgroup',
-        'utilde', 'undergroup',
-        'acute', 'vec', 'Overrightarrow',
-        'bar', 'overleftarrow', 'overrightarrow',
-        'breve', 'underleftarrow', 'underrightarrow',
-        'check', 'overleftharpoon', 'overrightharpoon',
-        'dot', 'overleftrightarrow', 'overbrace',
-        'ddot', 'underleftrightarrow', 'underbrace',
-        'grave', 'overline', 'overlinesegment',
-        'hat', 'underline', 'underlinesegment',
-        'widehat', 'widecheck'
-    ];
-    delimiters0 = [
-        'lparen', 'rparen', 'lceil', 'rceil', 'uparrow',
-        'lbrack', 'rbrack', 'lfloor', 'rfloor', 'downarrow',
-        'lbrace', 'rbrace', 'lmoustache', 'rmoustache', 'updownarrow',
-        'langle', 'rangle', 'lgroup', 'rgroup', 'Uparrow',
-        'vert', 'ulcorner', 'urcorner', 'Downarrow',
-        'Vert', 'llcorner', 'lrcorner', 'Updownarrow',
-        'lvert', 'rvert', 'lVert', 'rVert', 'backslash',
-        'lang', 'rang', 'lt', 'gt', 'llbracket', 'rrbracket', 'lBrace', 'rBrace'
-    ];
-    delimeterSizing0 = [
-        'left', 'big', 'bigl', 'bigm', 'bigr',
-        'middle', 'Big', 'Bigl', 'Bigm', 'Bigr',
-        'right', 'bigg', 'biggl', 'biggm', 'biggr',
-        'Bigg', 'Biggl', 'Biggm', 'Biggr'
-    ];
-    greekLetters0 = [
-        'Alpha', 'Beta', 'Gamma', 'Delta',
-        'Epsilon', 'Zeta', 'Eta', 'Theta',
-        'Iota', 'Kappa', 'Lambda', 'Mu',
-        'Nu', 'Xi', 'Omicron', 'Pi',
-        'Rho', 'Sigma', 'Tau', 'Upsilon',
-        'Phi', 'Chi', 'Psi', 'Omega',
-        'varGamma', 'varDelta', 'varTheta', 'varLambda',
-        'varXi', 'varPi', 'varSigma', 'varUpsilon',
-        'varPhi', 'varPsi', 'varOmega',
-        'alpha', 'beta', 'gamma', 'delta',
-        'epsilon', 'zeta', 'eta', 'theta',
-        'iota', 'kappa', 'lambda', 'mu',
-        'nu', 'xi', 'omicron', 'pi',
-        'rho', 'sigma', 'tau', 'upsilon',
-        'phi', 'chi', 'psi', 'omega',
-        'varepsilon', 'varkappa', 'vartheta', 'thetasym',
-        'varpi', 'varrho', 'varsigma', 'varphi',
-        'digamma'
-    ];
-    otherLetters0 = [
-        'imath', 'nabla', 'Im', 'Reals',
-        'jmath', 'partial', 'image', 'wp',
-        'aleph', 'Game', 'Bbbk', 'weierp',
-        'alef', 'Finv', 'N', 'Z',
-        'alefsym', 'cnums', 'natnums',
-        'beth', 'Complex', 'R',
-        'gimel', 'ell', 'Re',
-        'daleth', 'hbar', 'real',
-        'eth', 'hslash', 'reals'
-    ];
-    annotation1 = [
-        'cancel', 'overbrace',
-        'bcancel', 'underbrace',
-        'xcancel', 'not =',
-        'sout', 'boxed',
-        'tag', 'tag*'
-    ];
-    verticalLayout0 = ['atop'];
-    verticalLayout1 = ['substack'];
-    verticalLayout2 = ['stackrel', 'overset', 'underset', 'raisebox'];
-    overlap1 = ['mathllap', 'mathrlap', 'mathclap', 'llap', 'rlap', 'clap', 'smash'];
-    spacing0 = [
-        'thinspace', 'medspace', 'thickspace', 'enspace',
-        'quad', 'qquad', 'negthinspace', 'negmedspace',
-        'nobreakspace', 'negthickspace'
-    ];
-    spacing1 = [
-        'kern', 'mkern', 'mskip', 'hskip',
-        'hspace', 'hspace*', 'phantom', 'hphantom', 'vphantom'
-    ];
-    logicAndSetTheory0 = [
-        'forall', 'complement', 'therefore', 'emptyset',
-        'exists', 'subset', 'because', 'empty',
-        'exist', 'supset', 'mapsto', 'varnothing',
-        'nexists', 'mid', 'to', 'implies',
-        'in', 'land', 'gets', 'impliedby',
-        'isin', 'lor', 'leftrightarrow', 'iff',
-        'notin', 'ni', 'notni', 'neg', 'lnot'
-    ];
-    macros0 = [
-        'def', 'gdef', 'edef', 'xdef', 'let', 'futurelet', 'global',
-        'newcommand', 'renewcommand', 'providecommand',
-        'long', 'char', 'mathchoice', 'TextOrMath',
-        '@ifstar', '@ifnextchar', '@firstoftwo', '@secondoftwo',
-        'relax', 'expandafter', 'noexpand'
-    ];
-    bigOperators0 = [
-        'sum', 'prod', 'bigotimes', 'bigvee',
-        'int', 'coprod', 'bigoplus', 'bigwedge',
-        'iint', 'intop', 'bigodot', 'bigcap',
-        'iiint', 'smallint', 'biguplus', 'bigcup',
-        'oint', 'oiint', 'oiiint', 'bigsqcup'
-    ];
-    binaryOperators0 = [
-        'cdot', 'gtrdot', 'pmod',
-        'cdotp', 'intercal', 'pod',
-        'centerdot', 'land', 'rhd',
-        'circ', 'leftthreetimes', 'rightthreetimes',
-        'amalg', 'circledast', 'ldotp', 'rtimes',
-        'And', 'circledcirc', 'lor', 'setminus',
-        'ast', 'circleddash', 'lessdot', 'smallsetminus',
-        'barwedge', 'Cup', 'lhd', 'sqcap',
-        'bigcirc', 'cup', 'ltimes', 'sqcup',
-        'bmod', 'curlyvee', 'times',
-        'boxdot', 'curlywedge', 'mp', 'unlhd',
-        'boxminus', 'div', 'odot', 'unrhd',
-        'boxplus', 'divideontimes', 'ominus', 'uplus',
-        'boxtimes', 'dotplus', 'oplus', 'vee',
-        'bullet', 'doublebarwedge', 'otimes', 'veebar',
-        'Cap', 'doublecap', 'oslash', 'wedge',
-        'cap', 'doublecup', 'pm', 'plusmn', 'wr'
-    ];
-    fractions0 = ['over', 'above'];
-    fractions2 = ['frac', 'dfrac', 'tfrac', 'cfrac', 'genfrac'];
-    binomialCoefficients0 = ['choose'];
-    binomialCoefficients2 = ['binom', 'dbinom', 'tbinom', 'brace', 'brack'];
-    mathOperators0 = [
-        'arcsin', 'cotg', 'ln', 'det',
-        'arccos', 'coth', 'log', 'gcd',
-        'arctan', 'csc', 'sec', 'inf',
-        'arctg', 'ctg', 'sin', 'lim',
-        'arcctg', 'cth', 'sinh', 'liminf',
-        'arg', 'deg', 'sh', 'limsup',
-        'ch', 'dim', 'tan', 'max',
-        'cos', 'exp', 'tanh', 'min',
-        'cosec', 'hom', 'tg', 'Pr',
-        'cosh', 'ker', 'th', 'sup',
-        'cot', 'lg', 'argmax',
-        'argmin', 'limits'
-    ];
-    mathOperators1 = ['operatorname'];
-    sqrt1 = ['sqrt'];
-    relations0 = [
-        'eqcirc', 'lesseqgtr', 'sqsupset',
-        'eqcolon', 'lesseqqgtr', 'sqsupseteq',
-        'Eqcolon', 'lessgtr', 'Subset',
-        'eqqcolon', 'lesssim', 'subset',
-        'approx', 'Eqqcolon', 'll', 'subseteq', 'sube',
-        'approxeq', 'eqsim', 'lll', 'subseteqq',
-        'asymp', 'eqslantgtr', 'llless', 'succ',
-        'backepsilon', 'eqslantless', 'lt', 'succapprox',
-        'backsim', 'equiv', 'mid', 'succcurlyeq',
-        'backsimeq', 'fallingdotseq', 'models', 'succeq',
-        'between', 'frown', 'multimap', 'succsim',
-        'bowtie', 'ge', 'owns', 'Supset',
-        'bumpeq', 'geq', 'parallel', 'supset',
-        'Bumpeq', 'geqq', 'perp', 'supseteq',
-        'circeq', 'geqslant', 'pitchfork', 'supseteqq',
-        'colonapprox', 'gg', 'prec', 'thickapprox',
-        'Colonapprox', 'ggg', 'precapprox', 'thicksim',
-        'coloneq', 'gggtr', 'preccurlyeq', 'trianglelefteq',
-        'Coloneq', 'gt', 'preceq', 'triangleq',
-        'coloneqq', 'gtrapprox', 'precsim', 'trianglerighteq',
-        'Coloneqq', 'gtreqless', 'propto', 'varpropto',
-        'colonsim', 'gtreqqless', 'risingdotseq', 'vartriangle',
-        'Colonsim', 'gtrless', 'shortmid', 'vartriangleleft',
-        'cong', 'gtrsim', 'shortparallel', 'vartriangleright',
-        'curlyeqprec', 'in', 'sim', 'vcentcolon',
-        'curlyeqsucc', 'Join', 'simeq', 'vdash',
-        'dashv', 'le', 'smallfrown', 'vDash',
-        'dblcolon', 'leq', 'smallsmile', 'Vdash',
-        'doteq', 'leqq', 'smile', 'Vvdash',
-        'Doteq', 'leqslant', 'sqsubset',
-        'doteqdot', 'lessapprox', 'sqsubseteq'
-    ];
-    negatedRelations0 = [
-        'gnapprox', 'ngeqslant', 'nsubseteq', 'precneqq',
-        'gneq', 'ngtr', 'nsubseteqq', 'precnsim',
-        'gneqq', 'nleq', 'nsucc', 'subsetneq',
-        'gnsim', 'nleqq', 'nsucceq', 'subsetneqq',
-        'gvertneqq', 'nleqslant', 'nsupseteq', 'succnapprox',
-        'lnapprox', 'nless', 'nsupseteqq', 'succneqq',
-        'lneq', 'nmid', 'ntriangleleft', 'succnsim',
-        'lneqq', 'notin', 'ntrianglelefteq', 'supsetneq',
-        'lnsim', 'notni', 'ntriangleright', 'supsetneqq',
-        'lvertneqq', 'nparallel', 'ntrianglerighteq', 'varsubsetneq',
-        'ncong', 'nprec', 'nvdash', 'varsubsetneqq',
-        'ne', 'npreceq', 'nvDash', 'varsupsetneq',
-        'neq', 'nshortmid', 'nVDash', 'varsupsetneqq',
-        'ngeq', 'nshortparallel', 'nVdash',
-        'ngeqq', 'nsim', 'precnapprox'
-    ];
-    arrows0 = [
-        'circlearrowleft', 'leftharpoonup', 'rArr',
-        'circlearrowright', 'leftleftarrows', 'rarr',
-        'curvearrowleft', 'leftrightarrow', 'restriction',
-        'curvearrowright', 'Leftrightarrow', 'rightarrow',
-        'Darr', 'leftrightarrows', 'Rightarrow',
-        'dArr', 'leftrightharpoons', 'rightarrowtail',
-        'darr', 'leftrightsquigarrow', 'rightharpoondown',
-        'dashleftarrow', 'Lleftarrow', 'rightharpoonup',
-        'dashrightarrow', 'longleftarrow', 'rightleftarrows',
-        'downarrow', 'Longleftarrow', 'rightleftharpoons',
-        'Downarrow', 'longleftrightarrow', 'rightrightarrows',
-        'downdownarrows', 'Longleftrightarrow', 'rightsquigarrow',
-        'downharpoonleft', 'longmapsto', 'Rrightarrow',
-        'downharpoonright', 'longrightarrow', 'Rsh',
-        'gets', 'Longrightarrow', 'searrow',
-        'Harr', 'looparrowleft', 'swarrow',
-        'hArr', 'looparrowright', 'to',
-        'harr', 'Lrarr', 'twoheadleftarrow',
-        'hookleftarrow', 'lrArr', 'twoheadrightarrow',
-        'hookrightarrow', 'lrarr', 'Uarr',
-        'iff', 'Lsh', 'uArr',
-        'impliedby', 'mapsto', 'uarr',
-        'implies', 'nearrow', 'uparrow',
-        'Larr', 'nleftarrow', 'Uparrow',
-        'lArr', 'nLeftarrow', 'updownarrow',
-        'larr', 'nleftrightarrow', 'Updownarrow',
-        'leadsto', 'nLeftrightarrow', 'upharpoonleft',
-        'leftarrow', 'nrightarrow', 'upharpoonright',
-        'Leftarrow', 'nRightarrow', 'upuparrows',
-        'leftarrowtail', 'nwarrow', 'leftharpoondown', 'Rarr'
-    ];
-    extensibleArrows1 = [
-        'xleftarrow', 'xrightarrow',
-        'xLeftarrow', 'xRightarrow',
-        'xleftrightarrow', 'xLeftrightarrow',
-        'xhookleftarrow', 'xhookrightarrow',
-        'xtwoheadleftarrow', 'xtwoheadrightarrow',
-        'xleftharpoonup', 'xrightharpoonup',
-        'xleftharpoondown', 'xrightharpoondown',
-        'xleftrightharpoons', 'xrightleftharpoons',
-        'xtofrom', 'xmapsto',
-        'xlongequal'
-    ];
-    braketNotation1 = ['bra', 'Bra', 'ket', 'Ket', 'braket'];
-    classAssignment1 = [
-        'mathbin', 'mathclose', 'mathinner', 'mathop',
-        'mathopen', 'mathord', 'mathpunct', 'mathrel'
-    ];
-    color2 = ['color', 'textcolor', 'colorbox'];
-    font0 = ['rm', 'bf', 'it', 'sf', 'tt'];
-    font1 = [
-        'mathrm', 'mathbf', 'mathit',
-        'mathnormal', 'textbf', 'textit',
-        'textrm', 'bold', 'Bbb',
-        'textnormal', 'boldsymbol', 'mathbb',
-        'text', 'bm', 'frak',
-        'mathsf', 'mathtt', 'mathfrak',
-        'textsf', 'texttt', 'mathcal', 'mathscr'
-    ];
-    size0 = [
-        'Huge', 'huge', 'LARGE', 'Large', 'large',
-        'normalsize', 'small', 'footnotesize', 'scriptsize', 'tiny'
-    ];
-    style0 = [
-        'displaystyle', 'textstyle', 'scriptstyle', 'scriptscriptstyle',
-        'limits', 'nolimits', 'verb'
-    ];
-    symbolsAndPunctuation0 = [
-        'cdots', 'LaTeX',
-        'ddots', 'TeX',
-        'ldots', 'nabla',
-        'vdots', 'infty',
-        'dotsb', 'infin',
-        'dotsc', 'checkmark',
-        'dotsi', 'dag',
-        'dotsm', 'dagger',
-        'dotso',
-        'sdot', 'ddag',
-        'mathellipsis', 'ddagger',
-        'Box', 'Dagger',
-        'lq', 'square', 'angle',
-        'blacksquare', 'measuredangle',
-        'rq', 'triangle', 'sphericalangle',
-        'triangledown', 'top',
-        'triangleleft', 'bot',
-        'triangleright',
-        'colon', 'bigtriangledown',
-        'backprime', 'bigtriangleup', 'pounds',
-        'prime', 'blacktriangle', 'mathsterling',
-        'blacktriangledown',
-        'blacktriangleleft', 'yen',
-        'blacktriangleright', 'surd',
-        'diamond', 'degree',
-        'Diamond',
-        'lozenge', 'mho',
-        'blacklozenge', 'diagdown',
-        'star', 'diagup',
-        'bigstar', 'flat',
-        'clubsuit', 'natural',
-        'copyright', 'clubs', 'sharp',
-        'circledR', 'diamondsuit', 'heartsuit',
-        'diamonds', 'hearts',
-        'circledS', 'spadesuit', 'spades',
-        'maltese', 'minuso'
-    ];
-    debugging0 = ['message', 'errmessage', 'show'];
-
     mathCompletionItems: vscode.CompletionItem[];
 
     EXCLUDE_GLOB: string;
 
     constructor() {
-        // \cmd
-        let c1 = Array.from(new Set(
-            [
-                ...this.delimiters0, ...this.delimeterSizing0,
-                ...this.greekLetters0, ...this.otherLetters0,
-                ...this.spacing0, ...this.verticalLayout0,
-                ...this.logicAndSetTheory0, ...this.macros0, ...this.bigOperators0,
-                ...this.binaryOperators0, ...this.binomialCoefficients0,
-                ...this.fractions0, ...this.mathOperators0,
-                ...this.relations0, ...this.negatedRelations0,
-                ...this.arrows0, ...this.font0, ...this.size0,
-                ...this.style0, ...this.symbolsAndPunctuation0,
-                ...this.debugging0
-            ]
-        )).map(cmd => {
-            let item = new vscode.CompletionItem('\\' + cmd, vscode.CompletionItemKind.Function);
-            item.insertText = cmd;
-            return item;
-        });
-        // \cmd{$1}
-        let c2 = Array.from(new Set(
-            [
-                ...this.accents1, ...this.annotation1,
-                ...this.verticalLayout1, ...this.overlap1, ...this.spacing1,
-                ...this.mathOperators1, ...this.sqrt1,
-                ...this.extensibleArrows1, ...this.font1,
-                ...this.braketNotation1, ...this.classAssignment1
-            ]
-        )).map(cmd => {
-            let item = new vscode.CompletionItem('\\' + cmd, vscode.CompletionItemKind.Function);
-            item.insertText = new vscode.SnippetString(`${cmd}\{$1\}`);
-            return item;
-        });
-        // \cmd{$1}{$2}
-        let c3 = Array.from(new Set(
-            [
-                ...this.verticalLayout2, ...this.binomialCoefficients2,
-                ...this.fractions2, ...this.color2
-            ]
-        )).map(cmd => {
-            let item = new vscode.CompletionItem('\\' + cmd, vscode.CompletionItemKind.Function);
-            item.insertText = new vscode.SnippetString(`${cmd}\{$1\}\{$2\}`);
-            return item;
-        });
-        let envSnippet = new vscode.CompletionItem('\\begin', vscode.CompletionItemKind.Snippet);
-        envSnippet.insertText = new vscode.SnippetString('begin{${1|aligned,alignedat,array,bmatrix,Bmatrix,cases,darray,dcases,gathered,matrix,pmatrix,rcases,smallmatrix,vmatrix,Vmatrix|}}\n\t$2\n\\end{$1}');
-
         // Pretend to support multi-workspacefolders
         let resource = null;
         if (vscode.workspace.workspaceFolders !== undefined && vscode.workspace.workspaceFolders.length > 0) {
@@ -380,10 +27,9 @@ class MdCompletionItemProvider implements vscode.CompletionItemProvider {
         }
 
         // Import macros from configurations
-        let configMacros = vscode.workspace.getConfiguration('markdown.extension.katex', resource).get<object>('macros');
-        var macroItems: vscode.CompletionItem[] = [];
-        for (const cmd of Object.keys(configMacros)) {
-            const expansion: string = configMacros[cmd];
+        const configMacros = vscode.workspace.getConfiguration('markdown.extension.katex', resource).get<Record<string, string>>('macros');
+        const macroItems: vscode.CompletionItem[] = [];
+        for (const [cmd, expansion] of Object.entries(configMacros)) {
             let item = new vscode.CompletionItem(cmd, vscode.CompletionItemKind.Function);
 
             // Find the number of arguments in the expansion
@@ -399,7 +45,7 @@ class MdCompletionItemProvider implements vscode.CompletionItemProvider {
             macroItems.push(item);
         }
 
-        this.mathCompletionItems = [...c1, ...c2, ...c3, envSnippet, ...macroItems];
+        this.mathCompletionItems = [...Katex_Command_Completion_Items, ...macroItems];
 
         // Sort
         this.mathCompletionItems.forEach(item => {
@@ -414,10 +60,10 @@ class MdCompletionItemProvider implements vscode.CompletionItemProvider {
 
         let excludePatterns = ['**/node_modules', '**/bower_components', '**/*.code-search'];
         if (vscode.workspace.getConfiguration('markdown.extension.completion', resource).get<boolean>('respectVscodeSearchExclude')) {
-            const configExclude = vscode.workspace.getConfiguration('search', resource).get<object>('exclude');
-            for (const key of Object.keys(configExclude)) {
-                if (configExclude[key] === true) {
-                    excludePatterns.push(key);
+            const configExclude = vscode.workspace.getConfiguration('search', resource).get<Record<string, boolean>>('exclude');
+            for (const [pattern, enabled] of Object.entries(configExclude)) {
+                if (enabled) {
+                    excludePatterns.push(pattern);
                 }
             }
         }
