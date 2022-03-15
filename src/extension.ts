fe80c74e8aa2ad5e6f1b03aaac29f209cb4fbebb
yzhang <583181285@qq.com>
2022-03-15 22:49:44 +0800

    🐛 #1092 update word pattern

--- src/extension.ts
+++ src/extension.ts
@@ -46,9 +46,10 @@ function activateMdExt(context: ExtensionContext) {
     // Auto show preview to side
     preview.activate(context);
 
-    // Allow `*` in word pattern for quick styling
+    // Allow `*` in word pattern for quick styling (toggle bold/italic without selection)
+    // original https://github.com/microsoft/vscode/blob/3e5c7e2c570a729e664253baceaf443b69e82da6/extensions/markdown-basics/language-configuration.json#L55
     languages.setLanguageConfiguration('markdown', {
-        wordPattern: /(-?\d*\.\d\w*)|([^\!\@\#\%\^\&\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s\，\。\《\》\？\；\：\‘\“\’\”\（\）\【\】\、]+)/g
+        wordPattern: /([*_]{1,2})?(\p{Alphabetic}|\p{Number}|\p{Nonspacing_Mark})(((\p{Alphabetic}|\p{Number}|\p{Nonspacing_Mark})|[_])?(\p{Alphabetic}|\p{Number}|\p{Nonspacing_Mark}))*\1/gu
     });
 
     showWelcome(context);
