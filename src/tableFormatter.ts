7af70f22f45db4f491bc6d4ecb115cddf7b5df0f
Robbert Verbruggen <verbruggenrf@outlook.com>
2022-04-16 12:40:14 +0200

    feat: format table with delimiter row without padding

--- src/tableFormatter.ts
+++ src/tableFormatter.ts
@@ -34,6 +34,13 @@ export function activate(_: ExtensionContext) {
 
 export function deactivate() { }
 
+enum ColumnAlignment {
+    None,
+    Left,
+    Center,
+    Right
+}
+
 class MarkdownDocumentFormatter implements DocumentFormattingEditProvider {
     public provideDocumentFormattingEdits(document: TextDocument, options: FormattingOptions, token: CancellationToken): TextEdit[] | Thenable<TextEdit[]> {
         let edits: TextEdit[] = [];
@@ -92,6 +99,13 @@ class MarkdownDocumentFormatter implements DocumentFormattingEditProvider {
     }
 
     private formatTable(text: string, doc: TextDocument, options: FormattingOptions) {
+        const cellPadding = ' ';
+        const cellPaddingLength = cellPadding.length * 2;
+
+        const delimiterRowNum = 1;
+
+        let delimiterRowNoPadding = workspace.getConfiguration('markdown.extension.tableFormatter').get<boolean>('delimiterRowNoPadding');
+
         let indentation = this.getTableIndentation(text, options);
 
         let rows: string[] = [];
@@ -126,13 +140,12 @@ class MarkdownDocumentFormatter implements DocumentFormattingEditProvider {
             while ((field = fieldRegExp.exec(row)) !== null) {
                 let cell = field[1].trim();
                 values.push(cell);
-
                 //// Calculate `colWidth`
-                //// Ignore length of dash-line to enable width reduction
-                if (num != 1) {
+                //// Ignore length of delimiter-line to enable width reduction
+                if (num != delimiterRowNum) {
                     //// Treat CJK characters as 2 English ones because of Unicode stuff
                     const numOfUnicodeChars = splitter.countGraphemes(cell);
-                    const width = cjkRegex.test(cell) ? numOfUnicodeChars + cell.match(cjkRegex).length : numOfUnicodeChars;
+                    const width = (cjkRegex.test(cell) ? numOfUnicodeChars + cell.match(cjkRegex).length : numOfUnicodeChars) + cellPaddingLength;
                     colWidth[i] = colWidth[i] > width ? colWidth[i] : width;
                 }
 
@@ -142,33 +155,58 @@ class MarkdownDocumentFormatter implements DocumentFormattingEditProvider {
         });
 
         // Normalize the num of hyphen, use Math.max to determine minimum length based on dash-line format
-        lines[1] = lines[1].map((cell, i) => {
+        lines[delimiterRowNum] = lines[delimiterRowNum].map((cell, i) => {
             if (/:-+:/.test(cell)) {
                 //:---:
-                colWidth[i] = Math.max(colWidth[i], 5);
-                colAlign[i] = 'c';
-                return ':' + '-'.repeat(colWidth[i] - 2) + ':';
+                const minimalWidth = delimiterRowNoPadding ? 5 : 5 + cellPaddingLength;
+                const numberOfColons = 2;
+
+                colWidth[i] = Math.max(colWidth[i], minimalWidth);
+                colAlign[i] = ColumnAlignment.Center;
+
+                let numberOfDelimiters = this.calculateNumberOfDelimiters(colWidth[i], numberOfColons, delimiterRowNoPadding, cellPaddingLength);
+
+                return ':' + '-'.repeat(numberOfDelimiters) + ':';
             } else if (/:-+/.test(cell)) {
                 //:---
-                colWidth[i] = Math.max(colWidth[i], 4);
-                colAlign[i] = 'l';
-                return ':' + '-'.repeat(colWidth[i] - 1);
+                const minimalWidth = delimiterRowNoPadding ? 4 : 4 + cellPaddingLength;
+                const numberOfColons = 1;
+
+                colWidth[i] = Math.max(colWidth[i], minimalWidth);
+                colAlign[i] = ColumnAlignment.Left;
+
+                let numberOfDelimiters = this.calculateNumberOfDelimiters(colWidth[i], numberOfColons, delimiterRowNoPadding, cellPaddingLength);
+
+                return ':' + '-'.repeat(numberOfDelimiters);
             } else if (/-+:/.test(cell)) {
                 //---:
-                colWidth[i] = Math.max(colWidth[i], 4);
-                colAlign[i] = 'r';
-                return '-'.repeat(colWidth[i] - 1) + ':';
+                const minimalWidth = delimiterRowNoPadding ? 4 : 4 + cellPaddingLength;
+                const numberOfColons = 1;
+
+                colWidth[i] = Math.max(colWidth[i], minimalWidth);
+                colAlign[i] = ColumnAlignment.Right;
+
+                let numberOfDelimiters = this.calculateNumberOfDelimiters(colWidth[i], numberOfColons, delimiterRowNoPadding, cellPaddingLength);
+
+                return '-'.repeat(numberOfDelimiters) + ':';
             } else if (/-+/.test(cell)) {
                 //---
-                colWidth[i] = Math.max(colWidth[i], 3);
-                colAlign[i] = 'l';
-                return '-'.repeat(colWidth[i]);
+                const minimalWidth = delimiterRowNoPadding ? 3 : 3 + cellPaddingLength;
+                const numberOfColons = 0;
+
+                colWidth[i] = Math.max(colWidth[i], minimalWidth);
+                colAlign[i] = ColumnAlignment.None;
+
+                let numberOfDelimiters = this.calculateNumberOfDelimiters(colWidth[i], numberOfColons, delimiterRowNoPadding, cellPaddingLength);
+
+                return '-'.repeat(numberOfDelimiters);
             } else {
-                colAlign[i] = 'l';
+                colAlign[i] = ColumnAlignment.None;
             }
         });
 
-        return lines.map(row => {
+        return lines.map((row, i) => {
+            let rowNum = i;
             let cells = row.map((cell, i) => {
                 const desiredWidth = colWidth[i];
                 let jsLength = splitter.splitGraphemes(cell + ' '.repeat(desiredWidth)).slice(0, desiredWidth).join('').length;
@@ -177,19 +215,34 @@ class MarkdownDocumentFormatter implements DocumentFormattingEditProvider {
                     jsLength -= cell.match(cjkRegex).length;
                 }
 
-                return this.alignText(cell, colAlign[i], jsLength);
+                if (delimiterRowNoPadding && rowNum === delimiterRowNum) {
+                    return this.alignText(cell, colAlign[i], jsLength, '');
+                }
+
+                return this.alignText(cell, colAlign[i], jsLength, cellPadding);
             });
-            return indentation + '| ' + cells.join(' | ') + ' |';
+
+            return indentation + '|' + cells.join('|') + '|';
         }).join(doc.eol === EndOfLine.LF ? '\n' : '\r\n');
     }
 
-    private alignText(text: string, align: string, length: number) {
-        if (align === 'c' && length > text.length) {
-            return (' '.repeat(Math.floor((length - text.length) / 2)) + text + ' '.repeat(length)).slice(0, length);
-        } else if (align === 'r') {
-            return (' '.repeat(length) + text).slice(-length);
+    private alignText(text: string, align: ColumnAlignment, length: number, padding: string) {
+        if (align === ColumnAlignment.Center && length > text.length) {
+            return (padding + ' '.repeat(Math.floor((length - text.length) / 2) - padding.length) + text + ' '.repeat(length) + padding).slice(0, length);
+        } else if (align === ColumnAlignment.Right) {
+            return (padding + ' '.repeat(length) + text + padding).slice(-length);
         } else {
-            return (text + ' '.repeat(length)).slice(0, length);
+            return (padding + text + ' '.repeat(length) + padding).slice(0, length);
         }
+    }
+
+    private calculateNumberOfDelimiters(columnWidth: number, numberOfColons: number, delimiterRowNoPadding: boolean, cellPaddingLength: number) {
+        let numberOfDelimiters = 0;
+
+        let paddingLength = delimiterRowNoPadding ? 0 : cellPaddingLength;
+
+        numberOfDelimiters = columnWidth - numberOfColons - paddingLength;
+
+        return numberOfDelimiters;
     }
 }
