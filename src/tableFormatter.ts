2529448382dac98050c901a54816d5251fd8f9b1
Yu Zhang <yzhang.life@outlook.com>
2022-08-13 12:39:40 +0800

    Update tableFormatter.ts

--- src/tableFormatter.ts
+++ src/tableFormatter.ts
@@ -119,7 +119,7 @@ class MarkdownDocumentFormatter implements vscode.DocumentFormattingEditProvider
         let rowsNoIndentPattern = new RegExp(/^\s*(\S.*)$/gum);
         const rows: string[] = Array.from(text.matchAll(rowsNoIndentPattern), (match) => match[1].trim());
 
-        // Desired width of each column
+        // Desired "visual" width of each column
         const colWidth: number[] = [];
         // Alignment of each column
         const colAlign: ("l" | "c" | "r")[] = [];
@@ -129,7 +129,7 @@ class MarkdownDocumentFormatter implements vscode.DocumentFormattingEditProvider
         // https://www.ling.upenn.edu/courses/Spring_2003/ling538/UnicodeRanges.html
         let cjkRegex = /[\u3000-\u9fff\uac00-\ud7af\uff01-\uff60]/g;
 
-        let lines = rows.map((row, num) => {
+        let lines = rows.map((row, iRow) => {
             // Normalize
             if (row.startsWith('|')) {
                 row = row.slice(1);
@@ -139,12 +139,17 @@ class MarkdownDocumentFormatter implements vscode.DocumentFormattingEditProvider
             }
 
             let values = [];
-            let i = 0;
+            let iCol = 0;
             for (const field of row.matchAll(fieldRegExp)) {
                 let cell = field[1].trim();
                 values.push(cell);
 
-                // Calculate column width.
+                // The length of the dash-line doesn't matter
+                if (iRow === 1) {
+                    continue;
+                }
+
+                // Calculate the desired "visual" column width.
                 // The following notes help to understand the precondition for our calculation.
                 // They don't reflect how text layout engines really work.
                 // For more information, please consult UAX #11.
@@ -153,60 +158,52 @@ class MarkdownDocumentFormatter implements vscode.DocumentFormattingEditProvider
                 // In typical fixed-width typesetting without ligature, one grapheme is finally mapped to one glyph.
                 // Such a glyph is usually the same width as an ASCII letter, but a CJK glyph is twice.
 
-                if (num === 1) {
-                    i++;
-                    continue; // Ignore the delimiter row.
-                }
-
                 const graphemeCount = splitter.countGraphemes(cell);
                 const cjkPoints = cell.match(cjkRegex);
                 const width = graphemeCount + (cjkPoints?.length ?? 0);
-                colWidth[i] = width;
+                colWidth[iCol] = Math.max(colWidth[iCol] || 0, width);
 
-                i++;
+                iCol++;
             }
             return values;
         });
 
-        // @ts-ignore What's this on earth?
-        // Normalize the num of hyphen, use Math.max to determine minimum length based on dash-line format
-        lines[1] = lines[1].map((cell, i) => {
+        // Normalize the num of hyphen according to the desired column length
+        lines[1] = lines[1].map((cell, iCol) => {
             if (/:-+:/.test(cell)) {
-                //:---:
-                colWidth[i] = Math.max(colWidth[i], 5);
-                colAlign[i] = 'c';
-                return ':' + '-'.repeat(colWidth[i] - 2) + ':';
+                // :---:
+                colWidth[iCol] = Math.max(colWidth[iCol], 5);
+                colAlign[iCol] = 'c';
+                return ':' + '-'.repeat(colWidth[iCol] - 2) + ':';
             } else if (/:-+/.test(cell)) {
-                //:---
-                colWidth[i] = Math.max(colWidth[i], 4);
-                colAlign[i] = 'l';
-                return ':' + '-'.repeat(colWidth[i] - 1);
+                // :---
+                colWidth[iCol] = Math.max(colWidth[iCol], 4);
+                colAlign[iCol] = 'l';
+                return ':' + '-'.repeat(colWidth[iCol] - 1);
             } else if (/-+:/.test(cell)) {
-                //---:
-                colWidth[i] = Math.max(colWidth[i], 4);
-                colAlign[i] = 'r';
-                return '-'.repeat(colWidth[i] - 1) + ':';
-            } else if (/-+/.test(cell)) {
-                //---
-                colWidth[i] = Math.max(colWidth[i], 3);
-                colAlign[i] = 'l';
-                return '-'.repeat(colWidth[i]);
+                // ---:
+                colWidth[iCol] = Math.max(colWidth[iCol], 4);
+                colAlign[iCol] = 'r';
+                return '-'.repeat(colWidth[iCol] - 1) + ':';
             } else {
-                colAlign[i] = 'l';
+                // ---
+                colWidth[iCol] = Math.max(colWidth[iCol], 3);
+                colAlign[iCol] = 'l';
+                return '-'.repeat(colWidth[iCol]);
             }
         });
 
         return lines.map(row => {
-            let cells = row.map((cell, i) => {
-                const desiredWidth = colWidth[i];
-                let jsLength = splitter.splitGraphemes(cell + ' '.repeat(desiredWidth)).slice(0, desiredWidth).join('').length;
+            let cells = row.map((cell, iCol) => {
+                const visualWidth = colWidth[iCol];
+                let jsLength = splitter.splitGraphemes(cell + ' '.repeat(visualWidth)).slice(0, visualWidth).join('').length;
 
                 const cjkPoints = cell.match(cjkRegex);
                 if (cjkPoints) {
                     jsLength -= cjkPoints.length;
                 }
 
-                return this.alignText(cell, colAlign[i], jsLength);
+                return this.alignText(cell, colAlign[iCol], jsLength);
             });
             return indentation + '| ' + cells.join(' | ') + ' |';
         }).join(doc.eol === vscode.EndOfLine.LF ? '\n' : '\r\n');
