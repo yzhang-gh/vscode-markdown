yzhang-gh/vscode-markdown

commit 432e134f3cab8e18823fcb2e677fef4dbf3f1140

🏗 Target VS Code 1.63 (#1119)

src/listEditing.ts
@@ -1,7 +1,7 @@
-'use strict'
-
 import { commands, ExtensionContext, Position, Range, Selection, TextEditor, window, workspace, WorkspaceEdit } from 'vscode';
 import { isInFencedCodeBlock, mathEnvCheck } from "./util/contextCheck";
+
+type IModifier = "ctrl" | "shift";
 
 export function activate(context: ExtensionContext) {
     context.subscriptions.push(
@@ -24,8 +24,8 @@ export function activate(context: ExtensionContext) {
 // The commands here are only bound to keys with `when` clause containing `editorTextFocus && !editorReadonly`. (package.json)
 // So we don't need to check whether `activeTextEditor` returns `undefined` in most cases.
 
-function onEnterKey(modifiers?: string) {
-    let editor = window.activeTextEditor;
+function onEnterKey(modifiers?: IModifier) {
+    const editor = window.activeTextEditor!;
     let cursorPos: Position = editor.selection.active;
     let line = editor.document.lineAt(cursorPos.line);
     let textBeforeCursor = line.text.substr(0, cursorPos.character);
@@ -37,7 +37,7 @@ function onEnterKey(modifiers?: string) {
     }
 
     if (modifiers == 'shift' || isInFencedCodeBlock(editor.document, cursorPos.line) || mathEnvCheck(editor.document, cursorPos)) {
-        return asNormal('enter', modifiers);
+        return asNormal(editor, 'enter', modifiers);
     }
 
     //// This is a possibility that the current line is a thematic break `<hr>` (GitHub #785)
@@ -48,7 +48,7 @@ function onEnterKey(modifiers?: string) {
             || lineTextNoSpace.replace(/\*/g, '').length === 0
         )
     ) {
-        return asNormal('enter', modifiers);
+        return asNormal(editor, 'enter', modifiers);
     }
 
     //// If it's an empty list item, remove it
@@ -58,10 +58,10 @@ function onEnterKey(modifiers?: string) {
             editBuilder.insert(line.range.end, '\n');
         }).then(() => {
             editor.revealRange(editor.selection);
-        }).then(() => fixMarker(findNextMarkerLineNumber()));
+        }).then(() => fixMarker(editor));
     }
 
-    let matches;
+    let matches: RegExpExecArray | null;
     if (/^> /.test(textBeforeCursor)) {
         // Block quotes
 
@@ -97,11 +97,11 @@ function onEnterKey(modifiers?: string) {
     } else if ((matches = /^(\s*[-+*] +(\[[ x]\] +)?)/.exec(textBeforeCursor)) !== null) {
         // Unordered list
         return editor.edit(editBuilder => {
-            editBuilder.insert(lineBreakPos, `\n${matches[1].replace('[x]', '[ ]')}`);
+            editBuilder.insert(lineBreakPos, `\n${matches![1].replace('[x]', '[ ]')}`);
         }).then(() => {
             // Fix cursor position
             if (modifiers == 'ctrl' && !cursorPos.isEqual(lineBreakPos)) {
-                let newCursorPos = cursorPos.with(line.lineNumber + 1, matches[1].length);
+                let newCursorPos = cursorPos.with(line.lineNumber + 1, matches![1].length);
                 editor.selection = new Selection(newCursorPos, newCursorPos);
             }
         }).then(() => { editor.revealRange(editor.selection) });
@@ -133,19 +133,19 @@ function onEnterKey(modifiers?: string) {
                 let newCursorPos = cursorPos.with(line.lineNumber + 1, toBeAdded.length);
                 editor.selection = new Selection(newCursorPos, newCursorPos);
             }
-        }).then(() => fixMarker()).then(() => { editor.revealRange(editor.selection) });
+        }).then(() => fixMarker(editor)).then(() => { editor.revealRange(editor.selection); });
     } else {
-        return asNormal('enter', modifiers);
+        return asNormal(editor, 'enter', modifiers);
     }
 }
 
-function onTabKey(modifiers?: string) {
-    let editor = window.activeTextEditor;
+function onTabKey(modifiers?: IModifier) {
+    const editor = window.activeTextEditor!;
     let cursorPos = editor.selection.start;
     let lineText = editor.document.lineAt(cursorPos.line).text;
 
     if (isInFencedCodeBlock(editor.document, cursorPos.line) || mathEnvCheck(editor.document, cursorPos)) {
-        return asNormal('tab', modifiers);
+        return asNormal(editor, 'tab', modifiers);
     }
 
     let match = /^\s*([-+*]|[0-9]+[.)]) +(\[[ x]\] +)?/.exec(lineText);
@@ -158,44 +158,44 @@ function onTabKey(modifiers?: string) {
         )
     ) {
         if (modifiers === 'shift') {
-            return outdent(editor).then(() => fixMarker());
+            return outdent(editor).then(() => fixMarker(editor));
         } else {
-            return indent(editor).then(() => fixMarker());
+            return indent(editor).then(() => fixMarker(editor));
         }
     } else {
-        return asNormal('tab', modifiers);
+        return asNormal(editor, 'tab', modifiers);
     }
 }
 
 function onBackspaceKey() {
-    let editor = window.activeTextEditor
+    const editor = window.activeTextEditor!;
     let cursor = editor.selection.active;
     let document = editor.document;
     let textBeforeCursor = document.lineAt(cursor.line).text.substr(0, cursor.character);
 
     if (isInFencedCodeBlock(document, cursor.line) || mathEnvCheck(editor.document, cursor)) {
-        return asNormal('backspace');
+        return asNormal(editor, 'backspace');
     }
 
     if (!editor.selection.isEmpty) {
-        return asNormal('backspace').then(() => fixMarker(findNextMarkerLineNumber()));
+        return asNormal(editor, 'backspace').then(() => fixMarker(editor));
     } else if (/^\s+([-+*]|[0-9]+[.)]) $/.test(textBeforeCursor)) {
         // e.g. textBeforeCursor === `  - `, `   1. `
-        return outdent(editor).then(() => fixMarker());
+        return outdent(editor).then(() => fixMarker(editor));
     } else if (/^([-+*]|[0-9]+[.)]) $/.test(textBeforeCursor)) {
         // e.g. textBeforeCursor === `- `, `1. `
         return editor.edit(editBuilder => {
             editBuilder.replace(new Range(cursor.with({ character: 0 }), cursor), ' '.repeat(textBeforeCursor.length))
-        }).then(() => fixMarker(findNextMarkerLineNumber()));
+        }).then(() => fixMarker(editor));
     } else if (/^\s*([-+*]|[0-9]+[.)]) +(\[[ x]\] )$/.test(textBeforeCursor)) {
         // e.g. textBeforeCursor === `- [ ]`, `1. [x]`, `  - [x]`
-        return deleteRange(editor, new Range(cursor.with({ character: textBeforeCursor.length - 4 }), cursor)).then(() => fixMarker(findNextMarkerLineNumber()));
+        return deleteRange(editor, new Range(cursor.with({ character: textBeforeCursor.length - 4 }), cursor)).then(() => fixMarker(editor));
     } else {
-        return asNormal('backspace');
+        return asNormal(editor, 'backspace');
     }
 }
 
-function asNormal(key: string, modifiers?: string) {
+function asNormal(editor: TextEditor, key: "backspace" | "enter" | "tab", modifiers?: IModifier) {
     switch (key) {
         case 'enter':
             if (modifiers === 'ctrl') {
@@ -207,7 +207,7 @@ function asNormal(key: string, modifiers?: string) {
             if (modifiers === 'shift') {
                 return commands.executeCommand('editor.action.outdentLines');
             } else if (
-                window.activeTextEditor.selection.isEmpty
+                editor.selection.isEmpty
                 && workspace.getConfiguration('emmet').get<boolean>('triggerExpansionOnTab')
             ) {
                 return commands.executeCommand('editor.emmet.action.expandAbbreviation');
@@ -227,11 +227,7 @@ function asNormal(key: string, modifiers?: string) {
  *
  * then indent the current line to align with the previous list item.
  */
-function indent(editor?: TextEditor) {
-    if (!editor) {
-        editor = window.activeTextEditor;
-    }
-
+function indent(editor: TextEditor) {
     if (workspace.getConfiguration("markdown.extension.list", editor.document.uri).get<string>("indentationSize") === "adaptive") {
         try {
             const selection = editor.selection;
@@ -255,11 +251,7 @@ function indent(editor?: TextEditor) {
 /**
  * Similar to `indent`-function
  */
-function outdent(editor?: TextEditor) {
-    if (!editor) {
-        editor = window.activeTextEditor;
-    }
-
+function outdent(editor: TextEditor) {
     if (workspace.getConfiguration("markdown.extension.list", editor.document.uri).get<string>("indentationSize") === "adaptive") {
         try {
             const selection = editor.selection;
@@ -301,16 +293,13 @@ function tryDetermineIndentationSize(editor: TextEditor, line: number, currentIn
 }
 
 /**
- * Returns the line number of the next ordered list item starting either from
- * the specified line or the beginning of the current selection.
+ * Returns the line index of the next ordered list item starting from the specified line.
+ *
+ * @param line
+ * Defaults to the beginning of the current primary selection (`editor.selection.start.line`)
+ * in order to find the first marker following either the cursor or the entire selected range.
  */
-function findNextMarkerLineNumber(line?: number): number {
-    let editor = window.activeTextEditor;
-    if (line === undefined) {
-        // Use start.line instead of active.line so that we can find the first
-        // marker following either the cursor or the entire selected range
-        line = editor.selection.start.line;
-    }
+function findNextMarkerLineNumber(editor: TextEditor, line = editor.selection.start.line): number | undefined {
     while (line < editor.document.lineCount) {
         const lineText = editor.document.lineAt(line).text;
 
@@ -360,14 +349,13 @@ function lookUpwardForMarker(editor: TextEditor, line: number, currentIndentatio
 /**
  * Fix ordered list marker *iteratively* starting from current line
  */
-export function fixMarker(line?: number) {
+export function fixMarker(editor: TextEditor, line?: number): Thenable<unknown> | void {
     if (!workspace.getConfiguration('markdown.extension.orderedList').get<boolean>('autoRenumber')) return;
     if (workspace.getConfiguration('markdown.extension.orderedList').get<string>('marker') == 'one') return;
 
-    let editor = window.activeTextEditor;
     if (line === undefined) {
         // Use either the first line containing an ordered list marker within the selection or the active line
-        line = findNextMarkerLineNumber();
+        line = findNextMarkerLineNumber(editor);
         if (line === undefined || line > editor.selection.end.line) {
             line = editor.selection.active.line;
         }
@@ -395,16 +383,16 @@ export function fixMarker(line?: number) {
                 // Add enough trailing spaces so that the text is still aligned at the same indentation level as it was previously, but always keep at least one space
                 fixedMarkerString += delimiter + " ".repeat(Math.max(1, listIndent - (fixedMarkerString + delimiter).length));
 
-                editBuilder.replace(new Range(line, leadingSpace.length, line, leadingSpace.length + listIndent), fixedMarkerString);
+                editBuilder.replace(new Range(line!, leadingSpace.length, line!, leadingSpace.length + listIndent), fixedMarkerString);
             },
             { undoStopBefore: false, undoStopAfter: false }
         ).then(() => {
-            let nextLine = line + 1;
+            let nextLine = line! + 1;
             let indentString = " ".repeat(listIndent);
             while (editor.document.lineCount > nextLine) {
                 const nextLineText = editor.document.lineAt(nextLine).text;
                 if (/^\s*[0-9]+[.)] +/.test(nextLineText)) {
-                    return fixMarker(nextLine);
+                    return fixMarker(editor, nextLine);
                 } else if (/^\s*$/.test(nextLineText)) {
                     nextLine++;
                 } else if (listIndent <= 4 && !nextLineText.startsWith(indentString)) {
@@ -427,12 +415,12 @@ function deleteRange(editor: TextEditor, range: Range): Thenable<boolean> {
     );
 }
 
-function checkTaskList() {
+function checkTaskList(): Thenable<unknown> | void {
     // - Look into selections for lines that could be checked/unchecked.
     // - The first matching line dictates the new state for all further lines.
     //   - I.e. if the first line is unchecked, only other unchecked lines will
     //     be considered, and vice versa.
-    let editor = window.activeTextEditor;
+    const editor = window.activeTextEditor!;
     const uncheckedRegex = /^(\s*([-+*]|[0-9]+[.)]) +\[) \]/
     const checkedRegex = /^(\s*([-+*]|[0-9]+[.)]) +\[)x\]/
     let toBeToggled: Position[] = [] // all spots that have an "[x]" resp. "[ ]" which should be toggled
@@ -448,7 +436,7 @@ function checkTaskList() {
                 continue;
             }
 
-            let matches: RegExpExecArray;
+            let matches: RegExpExecArray | null;
             if (
                 (matches = uncheckedRegex.exec(line.text))
                 && newState !== false
@@ -477,31 +465,37 @@ function checkTaskList() {
 }
 
 function onMoveLineUp() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.moveLinesUpAction')
-        .then(() => fixMarker());
+        .then(() => fixMarker(editor));
 }
 
 function onMoveLineDown() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.moveLinesDownAction')
-        .then(() => fixMarker(findNextMarkerLineNumber(window.activeTextEditor.selection.start.line - 1)));
+        .then(() => fixMarker(editor, findNextMarkerLineNumber(editor, editor.selection.start.line - 1)));
 }
 
 function onCopyLineUp() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.copyLinesUpAction')
-        .then(() => fixMarker());
+        .then(() => fixMarker(editor));
 }
 
 function onCopyLineDown() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.copyLinesDownAction')
-        .then(() => fixMarker());
+        .then(() => fixMarker(editor));
 }
 
 function onIndentLines() {
-    return indent().then(() => fixMarker());
+    const editor = window.activeTextEditor!;
+    return indent(editor).then(() => fixMarker(editor));
 }
 
 function onOutdentLines() {
-    return outdent().then(() => fixMarker());
+    const editor = window.activeTextEditor!;
+    return outdent(editor).then(() => fixMarker(editor));
 }
 
 export function deactivate() { }

.vscodeignore
@@ -3,6 +3,7 @@
 
 # Development tools, intermediate build results, and temporary files
 .vscode-test/
+build/**
 out/
 test/**
 tools/
@@ -19,7 +20,7 @@ src/
 .github/
 .vscode/
 .gitignore
-tsconfig.json
+**/tsconfig*.json
 package-json.lock
 webpack.config.js
 

build/build.js
@@ -1,0 +1,12 @@
+"use strict";
+
+const { createLogger } = require("./logger.js");
+
+const logger = createLogger("Build");
+
+logger.log("Started.");
+// This is not necessarily the last line of the log, as others may also register on the event.
+process.on("exit", (code) => logger.log(`${code ? "Failed" : "Passing"}.`));
+
+require("./compilation.js").run();
+require("./duplicate-changelog.js").run();

build/compilation.js
@@ -1,0 +1,134 @@
+// # Notes
+//
+// We adjust the configurations according to OS environment variables.
+// The `mode` reflects the "NODE_ENV", which is a convention established by Express.
+// Source map (`devtool`) is expensive and not needed on CI.
+//
+// The compilation starts when calling `webpack()` with a callback.
+// It is async, sequential. The callback is invoked only once.
+// https://webpack.js.org/api/node/#multicompiler
+
+"use strict";
+
+const webpack = require("webpack");
+const { createLogger } = require("./logger.js");
+
+const logger = createLogger("Compilation");
+
+/**
+ * @type {boolean}
+ * @see {@link https://docs.github.com/en/actions/reference/environment-variables}
+ * @see {@link https://github.com/actions/runner/blob/main/src/Runner.Sdk/ProcessInvoker.cs}
+ */
+const Env_Is_Ci = process.env["CI"] === "true" || process.env["GITHUB_ACTIONS"] === "true";
+
+/**
+ * Only distinguish "development" or not.
+ * @type {"development" | "production"}
+ */
+const Env_Mode = process.env["NODE_ENV"] === "development" ? "development" : "production";
+
+/**
+ * @type {webpack.StatsOptions}
+ */
+const Stats_Options = {
+    all: false,
+    assets: true,
+    children: true,
+    errors: true,
+    errorsCount: true,
+    outputPath: true,
+    timings: true,
+    version: true,
+    warnings: true,
+    warningsCount: true,
+};
+
+/**
+ * @param {webpack.StatsError} e
+ */
+const formatCompilationError = (e) => `
+ERROR @ ${e.moduleName} (${e.loc})
+${e.message}
+`;
+
+/**
+ * @param {webpack.StatsAsset} a
+ */
+const formatAssetInfo = (a) => `\
+${a.type} : ${a.name ?? "[no name]"} \
+: ${a.size} bytes \
+${a.emitted ? "[emitted]" : a.comparedForEmit ? "[compared for emit]" : ""}\
+`;
+
+/**
+ * Collects and formats stats summary by pre-order traversal.
+ * The stats tree should be relatively small in real world.
+ * @param {webpack.StatsCompilation} i - The beginning node.
+ */
+const formatStatsInfo = (i) => {
+    let r = i.name
+        ? `
+STATS @ ${i.name}
+${i.assets?.map(formatAssetInfo).join("\n") ?? "No asset."}
+Compiled in ${i.time} ms. Errors: ${i.errorsCount}. Warnings: ${i.warningsCount}.
+`
+        : "";
+
+    if (i.children?.length) {
+        for (const c of i.children) {
+            r += formatStatsInfo(c);
+        }
+    }
+
+    return r;
+};
+
+const run = () => {
+    logger.log(`Started. Mode: ${Env_Mode}. CI: ${Env_Is_Ci}.`);
+
+    const configs = require("../webpack.config.js");
+
+    for (const c of configs) {
+        c.mode = Env_Mode;
+
+        if (Env_Is_Ci) {
+            c.devtool = false;
+        }
+    }
+
+    webpack(configs, (err, stats) => {
+        // `!stats` is just to satisfy type-checking.
+        if (err || !stats) {
+            throw err;
+        }
+
+        /** @type {Required<webpack.StatsCompilation>} */
+        // @ts-ignore Too hard to check type. Please debug to inspect it.
+        const info = stats.toJson(Stats_Options);
+
+        logger.append(`webpack ${info.version}`, true, true);
+
+        // All errors. Treat warning as error.
+        if (info.errorsCount || info.warningsCount) {
+            logger.append([...info.errors, ...info.warnings].map(formatCompilationError).join("\n"));
+            logger.append(`
+Errors: ${info.errorsCount}.
+Warnings: ${info.warningsCount}.
+`);
+        }
+
+        // Summary of each configuration.
+        logger.append(formatStatsInfo(info));
+
+        logger.flush();
+
+        if (info.errorsCount) {
+            throw new Error("{Compilation} Failed.");
+        }
+
+        logger.log("Passing.");
+    });
+};
+
+module.exports = Object.freeze({ run });

build/duplicate-changelog.js
@@ -1,0 +1,28 @@
+// # Notes
+//
+// vsce will modify `README.md` and `CHANGELOG.md` during packaging.
+// Thus, we create the `changes.md` for ours to consume.
+// Due to relative paths in the file, it has to be under the project root.
+
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const { createLogger } = require("./logger.js");
+
+const logger = createLogger("Duplicate Changelog");
+
+const run = () => {
+    logger.log("Started.");
+
+    const projectRoot = path.resolve(__dirname, "..");
+    const src = path.resolve(projectRoot, "CHANGELOG.md");
+    const dest = path.resolve(projectRoot, "changes.md");
+
+    logger.log(`\nFrom: ${src}\nTo:   ${dest}`);
+    fs.copyFileSync(src, dest);
+
+    logger.log("Passing.");
+};
+
+module.exports = Object.freeze({ run });

build/logger.js
@@ -1,0 +1,93 @@
+"use strict";
+
+class Logger {
+    /**
+     * The logger name. Read-only.
+     * @type {string}
+     */
+    #name;
+
+    /**
+     * The output buffer.
+     * @type {string[]}
+     */
+    #buffer;
+
+    /**
+     * @param {string} name - Human-readable name of the logger.
+     */
+    constructor(name) {
+        this.#name = name;
+
+        this.#buffer = [];
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    /**
+     * @param {string} message
+     * @param {boolean} withName
+     * @param {boolean} withTime
+     */
+    #format(message, withName, withTime) {
+        // https://2ality.com/2011/10/string-concatenation.html
+        let result = "";
+
+        if (withTime) {
+            result += `[${new Date().toISOString()}] `;
+        }
+
+        if (withName) {
+            result += `{${this.#name}} `;
+        }
+
+        result += message;
+
+        return result;
+    }
+
+    /**
+     * Adds a message to the log, which will show on a manual flush.
+     * A Line Feed will be appended automatically.
+     * @param {string} message
+     * @param {boolean} withName - `true` to prepend the logger name.
+     * @param {boolean} withTime - `true` to prepend timestamp.
+     */
+    append(message, withName = false, withTime = false) {
+        this.#buffer.push(this.#format(message, withName, withTime));
+    }
+
+    /**
+     * Flushes the output buffer.
+     */
+    flush() {
+        if (this.#buffer.length) {
+            console.log(this.#buffer.join("\n"));
+        }
+        this.#buffer.length = 0;
+    }
+
+    /**
+     * Writes a message to the console immediately, always with timestamp and the logger name.
+     * @param {string} message
+     */
+    log(message) {
+        console.log(this.#format(message, true, true));
+    }
+}
+
+/**
+ * @param {string} label - The logger name representing this output channel.
+ */
+const createLogger = (label) => {
+    const logger = new Logger(label);
+    process.on("exit", () => logger.flush());
+    return logger;
+};
+
+// Sort in alphabetical order.
+module.exports = Object.freeze({
+    createLogger,
+});

commit 2424080472b521cd1268061db150e504d24e7ae0

🐛 regression since 432e134

src/listEditing.ts
@@ -324,7 +324,7 @@ function tryDetermineIndentationSize(editor: TextEditor, line: number, currentIn
  * Defaults to the beginning of the current primary selection (`editor.selection.start.line`)
  * in order to find the first marker following either the cursor or the entire selected range.
  */
-function findNextMarkerLineNumber(editor: TextEditor, line = editor.selection.start.line): number | undefined {
+function findNextMarkerLineNumber(editor: TextEditor, line = editor.selection.start.line): number {
     while (line < editor.document.lineCount) {
         const lineText = editor.document.lineAt(line).text;
 
@@ -338,7 +338,7 @@ function findNextMarkerLineNumber(editor: TextEditor, line = editor.selection.st
         }
         line++;
     }
-    return undefined;
+    return -1;
 }
 
 /**
@@ -390,13 +390,9 @@ export function fixMarker(editor: TextEditor, line?: number): Thenable<unknown> 
     if (workspace.getConfiguration('markdown.extension.orderedList').get<string>('marker') == 'one') return;
 
     if (line === undefined) {
-        // Use either the first line containing an ordered list marker within the selection or the active line
         line = findNextMarkerLineNumber(editor);
-        if (line === undefined || line > editor.selection.end.line) {
-            line = editor.selection.active.line;
-        }
     }
-    if (line < 0 || editor.document.lineCount <= line) {
+    if (line < 0 || line >= editor.document.lineCount) {
         return;
     }
 