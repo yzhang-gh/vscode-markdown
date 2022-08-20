yzhang-gh/vscode-markdown

commit 432e134f3cab8e18823fcb2e677fef4dbf3f1140

🏗 Target VS Code 1.63 (#1119)

src/listEditing.ts
@@ -1,7 +1,7 @@
-'use strict'
-
 import { commands, ExtensionContext, Position, Range, Selection, TextEditor, window, workspace, WorkspaceEdit } from 'vscode';
 import { isInFencedCodeBlock, mathEnvCheck } from "./util/contextCheck";
+
+type IModifier = "ctrl" | "shift";
 
 export function activate(context: ExtensionContext) {
     context.subscriptions.push(
@@ -24,8 +24,8 @@ export function activate(context: ExtensionContext) {
 // The commands here are only bound to keys with `when` clause containing `editorTextFocus && !editorReadonly`. (package.json)
 // So we don't need to check whether `activeTextEditor` returns `undefined` in most cases.
 
-function onEnterKey(modifiers?: string) {
-    let editor = window.activeTextEditor;
+function onEnterKey(modifiers?: IModifier) {
+    const editor = window.activeTextEditor!;
     let cursorPos: Position = editor.selection.active;
     let line = editor.document.lineAt(cursorPos.line);
     let textBeforeCursor = line.text.substr(0, cursorPos.character);
@@ -37,7 +37,7 @@ function onEnterKey(modifiers?: string) {
     }
 
     if (modifiers == 'shift' || isInFencedCodeBlock(editor.document, cursorPos.line) || mathEnvCheck(editor.document, cursorPos)) {
-        return asNormal('enter', modifiers);
+        return asNormal(editor, 'enter', modifiers);
     }
 
     //// This is a possibility that the current line is a thematic break `<hr>` (GitHub #785)
@@ -48,7 +48,7 @@ function onEnterKey(modifiers?: string) {
             || lineTextNoSpace.replace(/\*/g, '').length === 0
         )
     ) {
-        return asNormal('enter', modifiers);
+        return asNormal(editor, 'enter', modifiers);
     }
 
     //// If it's an empty list item, remove it
@@ -58,10 +58,10 @@ function onEnterKey(modifiers?: string) {
             editBuilder.insert(line.range.end, '\n');
         }).then(() => {
             editor.revealRange(editor.selection);
-        }).then(() => fixMarker(findNextMarkerLineNumber()));
+        }).then(() => fixMarker(editor));
     }
 
-    let matches;
+    let matches: RegExpExecArray | null;
     if (/^> /.test(textBeforeCursor)) {
         // Block quotes
 
@@ -97,11 +97,11 @@ function onEnterKey(modifiers?: string) {
     } else if ((matches = /^(\s*[-+*] +(\[[ x]\] +)?)/.exec(textBeforeCursor)) !== null) {
         // Unordered list
         return editor.edit(editBuilder => {
-            editBuilder.insert(lineBreakPos, `\n${matches[1].replace('[x]', '[ ]')}`);
+            editBuilder.insert(lineBreakPos, `\n${matches![1].replace('[x]', '[ ]')}`);
         }).then(() => {
             // Fix cursor position
             if (modifiers == 'ctrl' && !cursorPos.isEqual(lineBreakPos)) {
-                let newCursorPos = cursorPos.with(line.lineNumber + 1, matches[1].length);
+                let newCursorPos = cursorPos.with(line.lineNumber + 1, matches![1].length);
                 editor.selection = new Selection(newCursorPos, newCursorPos);
             }
         }).then(() => { editor.revealRange(editor.selection) });
@@ -133,19 +133,19 @@ function onEnterKey(modifiers?: string) {
                 let newCursorPos = cursorPos.with(line.lineNumber + 1, toBeAdded.length);
                 editor.selection = new Selection(newCursorPos, newCursorPos);
             }
-        }).then(() => fixMarker()).then(() => { editor.revealRange(editor.selection) });
+        }).then(() => fixMarker(editor)).then(() => { editor.revealRange(editor.selection); });
     } else {
-        return asNormal('enter', modifiers);
+        return asNormal(editor, 'enter', modifiers);
     }
 }
 
-function onTabKey(modifiers?: string) {
-    let editor = window.activeTextEditor;
+function onTabKey(modifiers?: IModifier) {
+    const editor = window.activeTextEditor!;
     let cursorPos = editor.selection.start;
     let lineText = editor.document.lineAt(cursorPos.line).text;
 
     if (isInFencedCodeBlock(editor.document, cursorPos.line) || mathEnvCheck(editor.document, cursorPos)) {
-        return asNormal('tab', modifiers);
+        return asNormal(editor, 'tab', modifiers);
     }
 
     let match = /^\s*([-+*]|[0-9]+[.)]) +(\[[ x]\] +)?/.exec(lineText);
@@ -158,44 +158,44 @@ function onTabKey(modifiers?: string) {
         )
     ) {
         if (modifiers === 'shift') {
-            return outdent(editor).then(() => fixMarker());
+            return outdent(editor).then(() => fixMarker(editor));
         } else {
-            return indent(editor).then(() => fixMarker());
+            return indent(editor).then(() => fixMarker(editor));
         }
     } else {
-        return asNormal('tab', modifiers);
+        return asNormal(editor, 'tab', modifiers);
     }
 }
 
 function onBackspaceKey() {
-    let editor = window.activeTextEditor
+    const editor = window.activeTextEditor!;
     let cursor = editor.selection.active;
     let document = editor.document;
     let textBeforeCursor = document.lineAt(cursor.line).text.substr(0, cursor.character);
 
     if (isInFencedCodeBlock(document, cursor.line) || mathEnvCheck(editor.document, cursor)) {
-        return asNormal('backspace');
+        return asNormal(editor, 'backspace');
     }
 
     if (!editor.selection.isEmpty) {
-        return asNormal('backspace').then(() => fixMarker(findNextMarkerLineNumber()));
+        return asNormal(editor, 'backspace').then(() => fixMarker(editor));
     } else if (/^\s+([-+*]|[0-9]+[.)]) $/.test(textBeforeCursor)) {
         // e.g. textBeforeCursor === `  - `, `   1. `
-        return outdent(editor).then(() => fixMarker());
+        return outdent(editor).then(() => fixMarker(editor));
     } else if (/^([-+*]|[0-9]+[.)]) $/.test(textBeforeCursor)) {
         // e.g. textBeforeCursor === `- `, `1. `
         return editor.edit(editBuilder => {
             editBuilder.replace(new Range(cursor.with({ character: 0 }), cursor), ' '.repeat(textBeforeCursor.length))
-        }).then(() => fixMarker(findNextMarkerLineNumber()));
+        }).then(() => fixMarker(editor));
     } else if (/^\s*([-+*]|[0-9]+[.)]) +(\[[ x]\] )$/.test(textBeforeCursor)) {
         // e.g. textBeforeCursor === `- [ ]`, `1. [x]`, `  - [x]`
-        return deleteRange(editor, new Range(cursor.with({ character: textBeforeCursor.length - 4 }), cursor)).then(() => fixMarker(findNextMarkerLineNumber()));
+        return deleteRange(editor, new Range(cursor.with({ character: textBeforeCursor.length - 4 }), cursor)).then(() => fixMarker(editor));
     } else {
-        return asNormal('backspace');
+        return asNormal(editor, 'backspace');
     }
 }
 
-function asNormal(key: string, modifiers?: string) {
+function asNormal(editor: TextEditor, key: "backspace" | "enter" | "tab", modifiers?: IModifier) {
     switch (key) {
         case 'enter':
             if (modifiers === 'ctrl') {
@@ -207,7 +207,7 @@ function asNormal(key: string, modifiers?: string) {
             if (modifiers === 'shift') {
                 return commands.executeCommand('editor.action.outdentLines');
             } else if (
-                window.activeTextEditor.selection.isEmpty
+                editor.selection.isEmpty
                 && workspace.getConfiguration('emmet').get<boolean>('triggerExpansionOnTab')
             ) {
                 return commands.executeCommand('editor.emmet.action.expandAbbreviation');
@@ -227,11 +227,7 @@ function asNormal(key: string, modifiers?: string) {
  *
  * then indent the current line to align with the previous list item.
  */
-function indent(editor?: TextEditor) {
-    if (!editor) {
-        editor = window.activeTextEditor;
-    }
-
+function indent(editor: TextEditor) {
     if (workspace.getConfiguration("markdown.extension.list", editor.document.uri).get<string>("indentationSize") === "adaptive") {
         try {
             const selection = editor.selection;
@@ -255,11 +251,7 @@ function indent(editor?: TextEditor) {
 /**
  * Similar to `indent`-function
  */
-function outdent(editor?: TextEditor) {
-    if (!editor) {
-        editor = window.activeTextEditor;
-    }
-
+function outdent(editor: TextEditor) {
     if (workspace.getConfiguration("markdown.extension.list", editor.document.uri).get<string>("indentationSize") === "adaptive") {
         try {
             const selection = editor.selection;
@@ -301,16 +293,13 @@ function tryDetermineIndentationSize(editor: TextEditor, line: number, currentIn
 }
 
 /**
- * Returns the line number of the next ordered list item starting either from
- * the specified line or the beginning of the current selection.
+ * Returns the line index of the next ordered list item starting from the specified line.
+ *
+ * @param line
+ * Defaults to the beginning of the current primary selection (`editor.selection.start.line`)
+ * in order to find the first marker following either the cursor or the entire selected range.
  */
-function findNextMarkerLineNumber(line?: number): number {
-    let editor = window.activeTextEditor;
-    if (line === undefined) {
-        // Use start.line instead of active.line so that we can find the first
-        // marker following either the cursor or the entire selected range
-        line = editor.selection.start.line;
-    }
+function findNextMarkerLineNumber(editor: TextEditor, line = editor.selection.start.line): number | undefined {
     while (line < editor.document.lineCount) {
         const lineText = editor.document.lineAt(line).text;
 
@@ -360,14 +349,13 @@ function lookUpwardForMarker(editor: TextEditor, line: number, currentIndentatio
 /**
  * Fix ordered list marker *iteratively* starting from current line
  */
-export function fixMarker(line?: number) {
+export function fixMarker(editor: TextEditor, line?: number): Thenable<unknown> | void {
     if (!workspace.getConfiguration('markdown.extension.orderedList').get<boolean>('autoRenumber')) return;
     if (workspace.getConfiguration('markdown.extension.orderedList').get<string>('marker') == 'one') return;
 
-    let editor = window.activeTextEditor;
     if (line === undefined) {
         // Use either the first line containing an ordered list marker within the selection or the active line
-        line = findNextMarkerLineNumber();
+        line = findNextMarkerLineNumber(editor);
         if (line === undefined || line > editor.selection.end.line) {
             line = editor.selection.active.line;
         }
@@ -395,16 +383,16 @@ export function fixMarker(line?: number) {
                 // Add enough trailing spaces so that the text is still aligned at the same indentation level as it was previously, but always keep at least one space
                 fixedMarkerString += delimiter + " ".repeat(Math.max(1, listIndent - (fixedMarkerString + delimiter).length));
 
-                editBuilder.replace(new Range(line, leadingSpace.length, line, leadingSpace.length + listIndent), fixedMarkerString);
+                editBuilder.replace(new Range(line!, leadingSpace.length, line!, leadingSpace.length + listIndent), fixedMarkerString);
             },
             { undoStopBefore: false, undoStopAfter: false }
         ).then(() => {
-            let nextLine = line + 1;
+            let nextLine = line! + 1;
             let indentString = " ".repeat(listIndent);
             while (editor.document.lineCount > nextLine) {
                 const nextLineText = editor.document.lineAt(nextLine).text;
                 if (/^\s*[0-9]+[.)] +/.test(nextLineText)) {
-                    return fixMarker(nextLine);
+                    return fixMarker(editor, nextLine);
                 } else if (/^\s*$/.test(nextLineText)) {
                     nextLine++;
                 } else if (listIndent <= 4 && !nextLineText.startsWith(indentString)) {
@@ -427,12 +415,12 @@ function deleteRange(editor: TextEditor, range: Range): Thenable<boolean> {
     );
 }
 
-function checkTaskList() {
+function checkTaskList(): Thenable<unknown> | void {
     // - Look into selections for lines that could be checked/unchecked.
     // - The first matching line dictates the new state for all further lines.
     //   - I.e. if the first line is unchecked, only other unchecked lines will
     //     be considered, and vice versa.
-    let editor = window.activeTextEditor;
+    const editor = window.activeTextEditor!;
     const uncheckedRegex = /^(\s*([-+*]|[0-9]+[.)]) +\[) \]/
     const checkedRegex = /^(\s*([-+*]|[0-9]+[.)]) +\[)x\]/
     let toBeToggled: Position[] = [] // all spots that have an "[x]" resp. "[ ]" which should be toggled
@@ -448,7 +436,7 @@ function checkTaskList() {
                 continue;
             }
 
-            let matches: RegExpExecArray;
+            let matches: RegExpExecArray | null;
             if (
                 (matches = uncheckedRegex.exec(line.text))
                 && newState !== false
@@ -477,31 +465,37 @@ function checkTaskList() {
 }
 
 function onMoveLineUp() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.moveLinesUpAction')
-        .then(() => fixMarker());
+        .then(() => fixMarker(editor));
 }
 
 function onMoveLineDown() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.moveLinesDownAction')
-        .then(() => fixMarker(findNextMarkerLineNumber(window.activeTextEditor.selection.start.line - 1)));
+        .then(() => fixMarker(editor, findNextMarkerLineNumber(editor, editor.selection.start.line - 1)));
 }
 
 function onCopyLineUp() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.copyLinesUpAction')
-        .then(() => fixMarker());
+        .then(() => fixMarker(editor));
 }
 
 function onCopyLineDown() {
+    const editor = window.activeTextEditor!;
     return commands.executeCommand('editor.action.copyLinesDownAction')
-        .then(() => fixMarker());
+        .then(() => fixMarker(editor));
 }
 
 function onIndentLines() {
-    return indent().then(() => fixMarker());
+    const editor = window.activeTextEditor!;
+    return indent(editor).then(() => fixMarker(editor));
 }
 
 function onOutdentLines() {
-    return outdent().then(() => fixMarker());
+    const editor = window.activeTextEditor!;
+    return outdent(editor).then(() => fixMarker(editor));
 }
 
 export function deactivate() { }

.vscodeignore
@@ -3,6 +3,7 @@
 
 # Development tools, intermediate build results, and temporary files
 .vscode-test/
+build/**
 out/
 test/**
 tools/
@@ -19,7 +20,7 @@ src/
 .github/
 .vscode/
 .gitignore
-tsconfig.json
+**/tsconfig*.json
 package-json.lock
 webpack.config.js
 

build/build.js
@@ -1,0 +1,12 @@
+"use strict";
+
+const { createLogger } = require("./logger.js");
+
+const logger = createLogger("Build");
+
+logger.log("Started.");
+// This is not necessarily the last line of the log, as others may also register on the event.
+process.on("exit", (code) => logger.log(`${code ? "Failed" : "Passing"}.`));
+
+require("./compilation.js").run();
+require("./duplicate-changelog.js").run();

commit e88c31824a17d1cf6029d612cc6c1e5ca8bb75ca

✨ Add custom context key for onTabKey/onShiftTabKey key binding. (#1075)
* Add custom context key for the when key of onTabKey/onShiftTabKey command keybinding.

* Add prefix to custome context key name

* add context service layer

* create context service for markdown.extension.editor.cursor.inMarkdownList context

* put getting text later to avoid unnecessary computation

* handle context services as an array

* removed duplicated analyzing

* separate InFencedCode and InMathEnv contexts from InMarkdownList

* rename markdown.extension.editor.cursor.inMarkdownList to markdown.extension.editor.cursor.inList

* remove analyzation duplicating with when context

* fix a forgotten of 2nd previous commit

* merge context services to reduce duplicated callback handler and processing

* rename file names to kebab-case (responding https://markdown-all-in-one.github.io/docs/contributing/code-style/file.html)

* fix a mistake of 3rd previous commit

* 🚨 fix compiler warnings

* 🐛 #1155 renumber tab indented lists

* 🐛 regression since 432e134

* 🔇

(cherry picked from commit cb38589aa1bd19e84164032f7a3c968e5e7d5c17)
# to fix conflicts:
#     src/listEditing.ts

Co-authored-by: Yu Zhang <yzhang.life@outlook.com>

src/editor-context-service/context-service-in-fenced-code-block.ts
@@ -1,0 +1,32 @@
+'use strict'
+
+import { ExtensionContext, Position, TextDocument, window } from 'vscode';
+import { AbsContextService } from "./i-context-service";
+import { isInFencedCodeBlock } from "../util/contextCheck";
+
+export class ContextServiceEditorInFencedCodeBlock extends AbsContextService {
+    public onActivate(_context: ExtensionContext) {
+        // set initial state of context
+        this.setState(false);
+    }
+
+    public dispose(): void { }
+
+    public onDidChangeActiveTextEditor(document: TextDocument, cursorPos: Position) {
+        this.updateContextState(document, cursorPos);
+    }
+
+    public onDidChangeTextEditorSelection(document: TextDocument, cursorPos: Position) {
+        this.updateContextState(document, cursorPos);
+    }
+
+    private updateContextState(document: TextDocument, cursorPos: Position) {
+        if (isInFencedCodeBlock(document, cursorPos.line)) {
+            this.setState(true);
+        }
+        else {
+            this.setState(false);
+        }
+        return;
+    }
+}

src/editor-context-service/context-service-in-list.ts
@@ -1,0 +1,34 @@
+'use strict'
+
+import { ExtensionContext, Position, TextDocument, window } from 'vscode';
+import { AbsContextService } from "./i-context-service";
+
+export class ContextServiceEditorInList extends AbsContextService {
+    public onActivate(_context: ExtensionContext) {
+        // set initial state of context
+        this.setState(false);
+    }
+
+    public dispose(): void { }
+
+    public onDidChangeActiveTextEditor(document: TextDocument, cursorPos: Position) {
+        this.updateContextState(document, cursorPos);
+    }
+
+    public onDidChangeTextEditorSelection(document: TextDocument, cursorPos: Position) {
+        this.updateContextState(document, cursorPos);
+    }
+
+    private updateContextState(document: TextDocument, cursorPos: Position) {
+        let lineText = document.lineAt(cursorPos.line).text;
+
+        let inList = /^\s*([-+*]|[0-9]+[.)]) +(\[[ x]\] +)?/.test(lineText);
+        if (inList) {
+            this.setState(true);
+        }
+        else {
+            this.setState(false);
+        }
+        return;
+    }
+}

src/editor-context-service/context-service-in-math-env.ts
@@ -1,0 +1,32 @@
+'use strict'
+
+import { ExtensionContext, Position, TextDocument, window } from 'vscode';
+import { AbsContextService } from "./i-context-service";
+import { mathEnvCheck } from "../util/contextCheck";
+
+export class ContextServiceEditorInMathEn extends AbsContextService {
+    public onActivate(_context: ExtensionContext) {
+        // set initial state of context
+        this.setState(false);
+    }
+
+    public dispose(): void { }
+
+    public onDidChangeActiveTextEditor(document: TextDocument, cursorPos: Position) {
+        this.updateContextState(document, cursorPos);
+    }
+
+    public onDidChangeTextEditorSelection(document: TextDocument, cursorPos: Position) {
+        this.updateContextState(document, cursorPos);
+    }
+
+    private updateContextState(document: TextDocument, cursorPos: Position) {
+        if (mathEnvCheck(document, cursorPos)) {
+            this.setState(true);
+        }
+        else {
+            this.setState(false);
+        }
+        return;
+    }
+}

src/editor-context-service/i-context-service.ts
@@ -1,0 +1,53 @@
+'use strict'
+
+import { commands, ExtensionContext, Position, TextDocument } from 'vscode';
+import type IDisposable from "../IDisposable";
+
+interface IContextService extends IDisposable {
+    onActivate(context: ExtensionContext): void;
+
+    /**
+     * handler of onDidChangeActiveTextEditor
+     * implement this method to handle that event to update context state
+     */
+    onDidChangeActiveTextEditor(document: TextDocument, cursorPos: Position): void;
+    /**
+     * handler of onDidChangeTextEditorSelection
+     * implement this method to handle that event to update context state
+     */
+    onDidChangeTextEditorSelection(document: TextDocument, cursorPos: Position): void;
+}
+
+export abstract class AbsContextService implements IContextService {
+    constructor(contextName: string) {
+        this.contextName = contextName;
+    }
+
+    protected readonly contextName: string;
+
+    /**
+     * activate context service
+     * @param context ExtensionContext
+     */
+    public abstract onActivate(context: ExtensionContext): void;
+    public abstract dispose(): void;
+
+    /**
+     * default handler of onDidChangeActiveTextEditor, do nothing.
+     * override this method to handle that event to update context state.
+     */
+    public abstract onDidChangeActiveTextEditor(document: TextDocument, cursorPos: Position): void;
+
+    /**
+    * default handler of onDidChangeTextEditorSelection, do nothing.
+    * override this method to handle that event to update context state.
+    */
+    public abstract onDidChangeTextEditorSelection(document: TextDocument, cursorPos: Position): void;
+
+    /**
+     * set state of context
+     */
+    protected setState(state: any) {
+        commands.executeCommand('setContext', this.contextName, state);
+    }
+}

src/editor-context-service/manager.ts
@@ -1,0 +1,67 @@
+﻿'use strict'
+
+import type IDisposable from "../IDisposable";
+import { ExtensionContext, window } from 'vscode';
+import { AbsContextService } from "./i-context-service";
+import { ContextServiceEditorInList } from "./context-service-in-list";
+import { ContextServiceEditorInFencedCodeBlock } from "./context-service-in-fenced-code-block";
+import { ContextServiceEditorInMathEn } from "./context-service-in-math-env";
+
+export class ContextServiceManager implements IDisposable {
+    private readonly contextServices: Array<AbsContextService> = [];
+
+    public constructor() {
+        // push context services
+        this.contextServices.push(new ContextServiceEditorInList("markdown.extension.editor.cursor.inList"));
+        this.contextServices.push(new ContextServiceEditorInFencedCodeBlock("markdown.extension.editor.cursor.inFencedCodeBlock"));
+        this.contextServices.push(new ContextServiceEditorInMathEn("markdown.extension.editor.cursor.inMathEnv"));
+    }
+
+    public activate(context: ExtensionContext) {
+        for (const service of this.contextServices) {
+            service.onActivate(context);
+        }
+        // subscribe update handler for context
+        context.subscriptions.push(
+            window.onDidChangeActiveTextEditor(() => this.onDidChangeActiveTextEditor()),
+            window.onDidChangeTextEditorSelection(() => this.onDidChangeTextEditorSelection())
+        );
+    }
+
+    public dispose(): void {
+        while (this.contextServices.length > 0) {
+            const service = this.contextServices.pop();
+            service!.dispose();
+        }
+    }
+
+    private onDidChangeActiveTextEditor() {
+        const editor = window.activeTextEditor;
+        if (editor === undefined) {
+            return;
+        }
+
+        const cursorPos = editor.selection.start;
+        const document = editor.document;
+
+        for (const service of this.contextServices) {
+            service.onDidChangeActiveTextEditor(document, cursorPos);
+        }
+    }
+
+    private onDidChangeTextEditorSelection() {
+        const editor = window.activeTextEditor;
+        if (editor === undefined) {
+            return;
+        }
+
+        const cursorPos = editor.selection.start;
+        const document = editor.document;
+
+        for (const service of this.contextServices) {
+            service.onDidChangeTextEditorSelection(document, cursorPos);
+        }
+    }
+}
+
+export const contextServiceManager = new ContextServiceManager();

src/extension.ts
@@ -2,6 +2,7 @@
 
 import { ExtensionContext, languages, Uri, window, workspace } from 'vscode';
 import { configManager } from "./configuration/manager";
+import { contextServiceManager } from "./editor-context-service/manager"
 import { decorationManager } from "./theming/decorationManager";
 import * as completion from './completion';
 import * as formatting from './formatting';
@@ -20,7 +21,7 @@ export function activate(context: ExtensionContext) {
     configNls({ extensionContext: context });
 
     context.subscriptions.push(
-        configManager, decorationManager, commonMarkEngine, mdEngine
+        configManager, contextServiceManager, decorationManager, commonMarkEngine, mdEngine
     );
 
     activateMdExt(context);
@@ -29,6 +30,8 @@ export function activate(context: ExtensionContext) {
 }
 
 function activateMdExt(context: ExtensionContext) {
+    // Context services
+    contextServiceManager.activate(context);
     // Override `Enter`, `Tab` and `Backspace` keys
     listEditing.activate(context);
     // Shortcuts

src/listEditing.ts
@@ -36,7 +36,7 @@ function onEnterKey(modifiers?: IModifier) {
         lineBreakPos = line.range.end;
     }
 
-    if (modifiers == 'shift' || isInFencedCodeBlock(editor.document, cursorPos.line) || mathEnvCheck(editor.document, cursorPos)) {
+    if (modifiers == 'shift') {
         return asNormal(editor, 'enter', modifiers);
     }
 
@@ -169,10 +169,6 @@ function onTabKey(modifiers?: IModifier) {
     let cursorPos = editor.selection.start;
     let lineText = editor.document.lineAt(cursorPos.line).text;
 
-    if (isInFencedCodeBlock(editor.document, cursorPos.line) || mathEnvCheck(editor.document, cursorPos)) {
-        return asNormal(editor, 'tab', modifiers);
-    }
-
     let match = /^\s*([-+*]|[0-9]+[.)]) +(\[[ x]\] +)?/.exec(lineText);
     if (
         match
@@ -197,10 +193,6 @@ function onBackspaceKey() {
     let cursor = editor.selection.active;
     let document = editor.document;
     let textBeforeCursor = document.lineAt(cursor.line).text.substr(0, cursor.character);
-
-    if (isInFencedCodeBlock(document, cursor.line) || mathEnvCheck(editor.document, cursor)) {
-        return asNormal(editor, 'backspace');
-    }
 
     if (!editor.selection.isEmpty) {
         return asNormal(editor, 'backspace').then(() => fixMarker(editor));