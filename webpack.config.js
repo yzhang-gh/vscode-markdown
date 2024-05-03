b45691f1137d49407270462d45ee49a6bd252b6a
hill <hill@hilll.dev>
2024-05-03 19:13:02 +0000

    fix: make webpack copy the wasm module to the correct path

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,7 @@ const Config_Node = {
         filename: "[name].js",
         path: path.resolve(__dirname, "dist", "node"),
         library: { type: "commonjs2" },
+        webassemblyModuleFilename: "zola_slug_bg.wasm",
         // Don't use `absoluteResourcePath`, as it's often not a file system path.
         devtoolModuleFilenameTemplate: (info) => pathToFileURL(path.resolve(__dirname, info.resourcePath)).href,
     },
